013a61b41a8f0815b286b59473c443ff510a44ff
Esraa Jbara <jbesraa@gmail.com>
2021-06-17 16:06:46 +0200

    start databse before app

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+import "./aliases";
 import { getApp } from "./server";
+import { startDatabase } from "./database";
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = getApp();
 
-app.listen(port, () => {
-  console.log(
-    "CELO_UBI_ADDRESS",
-    process.env.CELO_UBI_ADDRESS,
-    "\nCELO_UBI_RPC_HOST",
-    process.env.CELO_UBI_RPC_HOST,
-    "\nCELO_UBI_MNEMONIC set",
-    !!process.env.CELO_UBI_MNEMONIC
-  );
-  console.log(`App listening at http://localhost:${port}`);
-});
+const runApp = () => 
+  app.listen(PORT, () => {
+    console.log(
+      "NODE_ENV:",
+      process.env.NODE_ENV,
+      "\nCELO_UBI_ADDRESS:",
+      process.env.CELO_UBI_ADDRESS,
+      "\nCELO_UBI_RPC_HOST:",
+      process.env.CELO_UBI_RPC_HOST,
+      "\nCELO_UBI_MNEMONIC set:",
+      !!process.env.CELO_UBI_MNEMONIC
+    );
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+
+export const startServerWithDatabase = (): void => {
+  startDatabase(runApp);
+};
+
+startServerWithDatabase();
