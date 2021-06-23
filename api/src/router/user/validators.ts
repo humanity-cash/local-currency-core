dba6b1b51ed98e8b5f4589cf50f1a55561ae0a43
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-06-23 16:20:58 +0200

    prettier

--- api/src/router/user/validators.ts
+++ api/src/router/user/validators.ts
@@ -1,44 +1,32 @@
-import { body, param } from 'express-validator';
-import { mwVaildator } from 'src/middlewares';
+import { body, param } from "express-validator";
+import { mwVaildator } from "src/middlewares";
 
-const idInParams = [
-	param("id").notEmpty(),
-	mwVaildator
-]
+const idInParams = [param("id").notEmpty(), mwVaildator];
 
-export const createUserSettlement = [ 
-	body("userId").isString(),
-	body("transactionId").isString(),
-	body("settlementAmount").isNumeric(),
-	mwVaildator
+export const createUserSettlement = [
+  body("userId").isString(),
+  body("transactionId").isString(),
+  body("settlementAmount").isNumeric(),
+  mwVaildator,
 ];
 
-export const createUser = [ 
-	body("userId").isString(),
-	mwVaildator
+export const createUser = [body("userId").isString(), mwVaildator];
+
+export const getUser = [...idInParams];
+
+export const getUserAuthorizations = [...idInParams];
+
+export const authorizeUser = [
+  body("userId").isString(),
+  body("transactionId").isString(),
+  body("authorizationAmount").isNumeric(),
+  mwVaildator,
 ];
 
-export const getUser = [ 
-	...idInParams
+export const deleteUserAuthorization = [
+  body("userId").isString(),
+  body("transactionId").isString(),
+  mwVaildator,
 ];
 
-export const getUserAuthorizations = [ 
-	...idInParams
-];
-
-export const authorizeUser = [ 
-	body("userId").isString(),
-	body("transactionId").isString(),
-	body("authorizationAmount").isNumeric(),
-	mwVaildator
-];
-
-export const deleteUserAuthorization = [ 
-	body("userId").isString(),
-	body("transactionId").isString(),
-	mwVaildator
-];
-
-export const getUserSettlements = [ 
-	...idInParams
-];
+export const getUserSettlements = [...idInParams];
