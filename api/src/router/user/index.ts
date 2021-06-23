87dc82eb2132599120d80d58327011c3248e54cf
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-06-23 15:39:20 +0200

    adapt changes from contracts

--- api/src/router/user/index.ts
+++ api/src/router/user/index.ts
@@ -1,20 +1,23 @@
-import express from 'express';
-import * as controller from './controller';
-import * as validators from './validators';
+import express from "express";
+import * as controller from "./controller";
+import * as validators from "./validators";
 
 const user = express();
 
-user.get("/users", controller.getAllUsers); 
+user.get("/users", controller.getAllUsers);
 user.get("/users/:id", validators.getUser, controller.getUser);
 user.post("/users", validators.createUser, controller.createUser);
 
-user.get("/users/:id/authorizations", validators.getUserAuthorizations, controller.getUserAuthorizations);
-user.post("/users/:id/authorizations", validators.authorizeUser, controller.authorizeUser);
-user.delete("/user/:id/authorizations", validators.deleteUserAuthorization, controller.deleteUserAuthorization); 
-
-user.get("/users/:id/settlements", validators.getUserSettlements, controller.getUserSettlements); 
+user.get(
+  "/users/:id/settlements",
+  validators.getUserSettlements,
+  controller.getUserSettlements
+);
 user.get("/users/all/settlements", controller.getAllUsersSettlements);
-user.post("/users/:id/settlements", validators.createUserSettlement, controller.createSettlementForUser);
+user.post(
+  "/users/:id/settlements",
+  validators.createUserSettlement,
+  controller.createSettlementForUser
+);
 
-
-export default user
+export default user;
--- api/src/service/PublicService.ts
+++ api/src/service/PublicService.ts
@@ -1,22 +1,16 @@
 import * as Kit from "@celo/contractkit";
-import {
-  Authorization, HealthResponse, Settlement, UBIBeneficiary
-} from "src/types";
+import { HealthResponse, Settlement, IWallet } from "src/types";
 import { toBytes32 } from "src/utils/crypto";
-import * as CeloUBI from "./celoubi";
+import * as contracts from "./contracts";
 
 export async function health(): Promise<HealthResponse> {
-  const kit = Kit.newKit(process.env.CELO_UBI_RPC_HOST);
+  const kit = Kit.newKit(process.env.LOCAL_CURRENCY_RPC_HOST);
   const promises = [
     kit.web3.eth.getBlockNumber(),
     kit.web3.eth.getChainId(),
     kit.web3.eth.getNodeInfo(),
-    this.disbursementWei(),
-    this.cUBIAuthToken(),
-    this.cUSDToken(),
-    this.reconciliationAccount(),
-    CeloUBI.getBeneficiaryCount(),
-    CeloUBI.owner(),
+    this.token(),
+    contracts.owner(),
   ];
   const results = await Promise.all(promises);
 
@@ -24,76 +18,45 @@ export async function health(): Promise<HealthResponse> {
     blockNumber: results[0],
     chainId: results[1],
     nodeInfo: results[2],
-    disbursementWei: results[3],
-    cUBIAuthToken: results[4],
-    cUSDToken: results[5],
-    reconciliationAccount: results[6],
-    countOfBeneficiaries: results[7],
-    owner: results[8],
+    token: results[3],
+    countOfWallets: results[4],
+    owner: results[5],
   };
   return response;
 }
 
-export async function disbursementWei(): Promise<number> {
-  return await CeloUBI.disbursementWei();
+export async function token(): Promise<string> {
+  return await contracts.token();
 }
 
-export async function cUSDToken(): Promise<string> {
-  return await CeloUBI.cUSDToken();
+export async function getWalletAddress(userId: string): Promise<string> {
+  return await contracts.getWalletAddress(toBytes32(userId));
 }
 
-export async function cUBIAuthToken(): Promise<string> {
-  return await CeloUBI.cUBIAuthToken();
+export async function balanceOfWallet(userId: string): Promise<string> {
+  return await contracts.balanceOfWallet(toBytes32(userId));
 }
 
-export async function reconciliationAccount(): Promise<string> {
-  return await CeloUBI.reconciliationAccount();
+export async function getWallet(userId: string): Promise<IWallet> {
+  const address = await this.walletAddress(userId);
+  const wallet: IWallet = await contracts.getWalletForAddress(address);
+  return wallet;
 }
 
-export async function beneficiaryAddress(userId: string): Promise<string> {
-  return await CeloUBI.beneficiaryAddress(toBytes32(userId));
-}
-
-export async function balanceOfUBIBeneficiary(userId: string): Promise<string> {
-  return await CeloUBI.balanceOfUBIBeneficiary(toBytes32(userId));
-}
-
-export async function authbalanceOfUBIBeneficiary(
-  userId: string
-): Promise<string> {
-  return await CeloUBI.authBalanceOfUBIBeneficiary(toBytes32(userId));
-}
-
-export async function getBeneficiary(userId: string): Promise<UBIBeneficiary> {
-  const address = await this.beneficiaryAddress(userId);
-  const beneficiary: UBIBeneficiary = await CeloUBI.getUBIBeneficiaryForAddress(
-    address
-  );
-  return beneficiary;
-}
-
-export async function getAllBeneficiaries(): Promise<UBIBeneficiary[]> {
-  const count = await CeloUBI.getBeneficiaryCount();
-  const users: UBIBeneficiary[] = [];
+export async function getAllBeneficiaries(): Promise<IWallet[]> {
+  const count = await contracts.getWalletCount();
+  const users: IWallet[] = [];
 
   for (let i = 0; i < count; i++) {
-    const address = await CeloUBI.getBeneficiaryAddressAtIndex(i);
-    const user: UBIBeneficiary = await CeloUBI.getUBIBeneficiaryForAddress(
-      address
-    );
+    const address = await contracts.getWalletAddressAtIndex(i);
+    const user: IWallet = await contracts.getWalletForAddress(address);
     users.push(user);
   }
   return users;
 }
 
-export async function getAuthorizationsForAddress(
-  address: string
-): Promise<Authorization[]> {
-  return await CeloUBI.getAuthorizationsForAddress(address);
-}
-
 export async function getSettlementsForAddress(
   address: string
 ): Promise<Settlement[]> {
-  return await CeloUBI.getSettlementsForAddress(address);
+  return await contracts.getSettlementsForAddress(address);
 }
