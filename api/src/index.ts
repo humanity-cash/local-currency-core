5d62fa57e74f474205ff4de2eb8d3561424bc63a
Esraa Jbara <jbesraa@gmail.com>
2021-06-24 15:06:34 +0300

    merge index and server into one file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,29 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
+import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
 import "./aliases";
-import { getApp } from "./server";
+import router from './router';
+import * as Controller from "./controllers";
 
-const PORT = process.env.PORT || 3000;
-const app = getApp();
+export const setupServer = () : Express => {
+  const app = express();
+ 
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+  app.use(express.json({type: "application/json"}));
+  app.use(cors());
+  app.set("x-powered-by", false);
+  app.use(router);
+  app.get("/health", (req:Request, res:Response) => {Controller.health(req, res)});
+  app.post("/reconcile", (req:Request, res:Response) => {Controller.reconciliation(req,res)});
+
+  return app;
+};
 
 const runServer = () => {
+	const PORT = process.env.PORT || 3000;
+	const app = setupServer();
 	app.listen(PORT, () => {
 		console.log(
 			"NODE_ENV:",
