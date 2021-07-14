693ddbb613f37105a4fc4d2c061fc96f4e74609a
Esraa Jbara <jbesraa@gmail.com>
2021-07-14 13:11:49 +0300

    add example schema+service

---
+++ api/test/example.integration.test.ts
@@ -0,0 +1,10 @@
+import { ExampleService } from '../src/database/service';
+
+describe("Example Service", () => {
+	it("Creates Example Successfully", async () => {
+		const response = await ExampleService.create({firstName: 'firstName', age:0});
+		expect(response.age).toEqual(0);
+		expect(response.firstName).toEqual('firstName');
+		expect(response.id).toBeDefined();
+	});
+});
