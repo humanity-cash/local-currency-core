c6832917b6e2c4174b38055badf1cfc3fdbd16a9
Esraa Jbara <jbesraa@gmail.com>
2021-07-15 17:13:17 +0300

    add routes

--- web-admin/src/router/index.tsx
+++ web-admin/src/router/index.tsx
@@ -1,13 +1,16 @@
 import {
 	BrowserRouter as Router, Route, Switch
 } from "react-router-dom";
+import { Sidebar } from "../components";
+import { ContractsScreen } from "../screens";
 
 const App = () => {
 	return (
 		<div>
 			<Router>
+				<Sidebar />
 				<Switch>
-					<Route path="/" exact component={() => <div>home page</div>}/>
+					<Route path="/contracts" exact component={ContractsScreen}/>
 					<Route path="/second" component={() => <div>second page</div>}/>
 				</Switch>
 			</Router>
