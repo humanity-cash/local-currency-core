2b09e803c0dd1c9d7ab50d17722351a19f4337f4
Esraa Jbara <jbesraa@gmail.com>
2021-07-15 17:13:57 +0300

    add contracts store

--- web-admin/src/store.ts
+++ web-admin/src/store.ts
@@ -1,1 +1,9 @@
-export { };
+import { createStore } from "react-hookstore";
+import { ContractsState } from "./types";
+
+export const CONTRACTS_STORE = 'contracts_stores';
+
+const contractsStoreInitialState: ContractsState  = {
+	data: []
+}
+createStore(CONTRACTS_STORE, contractsStoreInitialState);
