7af16fc487f8ebdc2d38581b64b604c92c3abe71
Esraa Jbara <jbesraa@gmail.com>
2021-07-16 14:49:42 +0300

    add tabs and tables

--- web-admin/src/types.ts
+++ web-admin/src/types.ts
@@ -15,3 +15,28 @@ export interface ModalState {
 	isOpen: boolean;
 	modalProps: {};
 }
+
+export interface BlockchainDataState {
+	data: BlockchainData[]
+}
+
+export interface BlockchainData {
+	from: string;
+	to: string;
+	time: number;
+	amount: number;
+	status: string;
+}
+
+export interface ACHData {
+	user: string;
+	type: 'Funding' | 'Redemption';
+	time: number;
+	amount: number;
+	bank: string;
+	status: string;
+}
+
+export interface ACHDataState {
+	data: ACHData[]
+}
