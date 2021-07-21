77dd325c4303a83e8c0b9e8b5a05257a62748165
Esraa Jbara <jbesraa@gmail.com>
2021-07-21 13:29:14 +0300

    add contracts basic functions

--- web-admin/src/api/index.ts
+++ web-admin/src/api/index.ts
@@ -1,13 +1,4 @@
-export { };
+import * as ContractsAPI from './contracts';
+import * as DowllaAPI from './dowlla';
 
-/**
-	Queries: 
-		1) List Contracts
-
-	Mutations:
-		1) Stop Contract
-		2) Start Contract
-		3  Burn Token
-		4) Mint Token
-		5) Configure Contract 
- */
+export { ContractsAPI, DowllaAPI };
---
+++ web-admin/src/consts.ts
@@ -0,0 +1,6 @@
+
+/**Routes */
+export const START_CONTRACT = '/contract/actions/start'
+export const STOP_CONTRACT = '/contract/actions/start'
+export const GET_CONTRACTS = '/contract/all'
+
