ff773dde376b0c8271197f551b190bee75dd97c5
Esraa Jbara <jbesraa@gmail.com>
2021-07-21 13:27:43 +0300

    login screen

--- web-admin/src/router/index.tsx
+++ web-admin/src/router/index.tsx
@@ -1,17 +1,18 @@
-import { Modal, Sidebar } from "components";
+import { Modal } from "components";
 import {
 	BrowserRouter as Router, Route, Switch
 } from "react-router-dom";
-import { ContractsScreen, DashboardScreen, TransactionsScreen } from "screens";
+import { ContractsScreen, DashboardScreen, LoginScreen, TransactionsScreen } from "screens";
 
 const App = () => {
 	return (
 		<div>
 			<Router>
-				<Sidebar />
+				{/* <Sidebar /> */}
 				<Modal />
 				<Switch>
 					<Route path="/contracts" exact component={ContractsScreen}/>
+					<Route path="/login" exact component={LoginScreen}/>
 					<Route path="/" exact component={DashboardScreen}/>
 					<Route path="/ach/transactions" component={TransactionsScreen.ACHTransactionsTable}/>
 					<Route path="/bc/transactions" component={TransactionsScreen.BlockchainTransactionsTable}/>
