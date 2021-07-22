4289fcb72821b41e22268ffb5cdd6a3625f36613
Esraa Jbara <jbesraa@gmail.com>
2021-07-22 16:19:18 +0300

    integrate cognito/auth

--- web-admin/src/App.tsx
+++ web-admin/src/App.tsx
@@ -1,8 +1,25 @@
-import Router from './router';
+import AuthProvider, { AuthIsNotSignedIn, AuthIsSignedIn } from 'context/auth';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { NotProtectedRoutes, ProtectedRoutes } from './router';
 
 const App = () => {
+
   return (
-    <Router />
+    <Router>
+      <AuthProvider>
+        <AuthIsNotSignedIn>
+          <NotProtectedRoutes />
+        </AuthIsNotSignedIn>
+        <AuthIsSignedIn>
+          <ProtectedRoutes />
+        </AuthIsSignedIn>
+        <ToastContainer />
+      </AuthProvider>
+    </Router>
   );
 }
 
