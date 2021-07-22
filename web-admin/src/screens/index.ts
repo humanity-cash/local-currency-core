677af844312943da89d8e4b5346af1299a2f8bb8
Esraa Jbara <jbesraa@gmail.com>
2021-07-22 16:18:18 +0300

    add changepassword screen

--- web-admin/src/screens/index.ts
+++ web-admin/src/screens/index.ts
@@ -1,7 +1,8 @@
 import ContractsScreen from './contracts';
 import DashboardScreen from './dashboard';
 import LoginScreen from './login';
+import ChangePasswordScreen from './login/ChangePassword';
 import * as TransactionsScreen from './transactions';
 
-export { ContractsScreen, DashboardScreen, TransactionsScreen, LoginScreen };
+export { ChangePasswordScreen, ContractsScreen, DashboardScreen, TransactionsScreen, LoginScreen };
 
