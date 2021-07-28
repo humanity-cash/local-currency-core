a7d257eec77dcfa4e97a168892790b8a25964afe
Esraa Jbara <jbesraa@gmail.com>
2021-07-28 16:21:15 +0300

    organize data

--- web-admin/src/mock.ts
+++ web-admin/src/mock.ts
@@ -1,35 +1,143 @@
-interface ContractData {
-	name: string;
-	address: string;
-	deployedAt: number;
-	status: number;
-	actions: number;
-}
+import { ACHData, BlockchainData, ContractData } from "types";
 
-function createMockContractsData(name: string, address: string, deployedAt: number, status: number, actions: number): ContractData {
-	return { name, address, deployedAt, status, actions };
+const createMockContractsData = 
+	(name: string, address: string, deployedAt: number, status: number, version: number): ContractData => {
+
+	return { name, address, deployedAt, status, version };
 }
 
 export const MockContractsData = [
-	createMockContractsData('Token', 'IN', 1324171354, 3287263, 3287263),
-	createMockContractsData('Wallet', 'CN', 1403500365, 9596961, 3287263),
-	createMockContractsData('Controller', 'IT', 60483973, 301340, 3287263),
+	createMockContractsData('Token', '0x0001', 1324171354, 1, 1.04),
+	createMockContractsData('Wallet', '0x0001', 1403500365, 0, 0.15),
+	createMockContractsData('Controller', '0x0001', 60483973, 1, 0.8),
 ];
 
-function createMockACHData(user: string, type: string, time: number, status: number, amount: number, bank: string) {
-	return { user, type, time, status, amount, bank };
-}
-
-function createMockBCData(from: string, to: string, time: number, status: number, amount: number) {
-	return { from, to, time, status, amount };
-}
-
-export const MockACHData = [
-	createMockACHData('user x', 'Funding', 1324171354, 0, 3287263, 'bank'),
-	createMockACHData('user 1', 'Redepmtion', 1324171354, 0, 3287263, 'bank'),
+export const MockACHData: ACHData[] = [
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Success', transactionId: 234223
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Success', transactionId: 234223
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Success', transactionId: 234223
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Success', transactionId: 234223
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Success', transactionId: 234223
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Pending', transactionId: 254343
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Pending', transactionId: 254343
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Pending', transactionId: 254343
+	},
+	{ userEmail: 'user@email.io', type: 'Funding', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Pending', transactionId: 254343
+	},
+	{ userEmail: 'user@email.io', type: 'Redemption', createdAt: 0, 
+		confirmedAt: 0, amount: 20, bank: 'Bank Of Country', bankAccount: '1G1C1G2343ER',
+		status: 'Fail', transactionId: 234343
+	},
 ];
 
-export const MockBCData = [
-	createMockBCData('0x1', '0x2', 1324171354, 0, 3287263),
-	createMockBCData('0x2', '0x1', 1324171354, 0, 3287263),
+export const MockBCData: BlockchainData[] = [
+	{
+		transactionHash: '0x10000010000000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Fail',
+		isToMerchant: 0,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
+	{
+		transactionHash: '0x10000000030000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Success',
+		isToMerchant: 1,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
+	{
+		transactionHash: '0x10000000030000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Success',
+		isToMerchant: 1,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
+	{
+		transactionHash: '0x10000000030000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Success',
+		isToMerchant: 1,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
+	{
+		transactionHash: '0x10000000030000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Success',
+		isToMerchant: 1,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
+	{
+		transactionHash: '0x10000000030000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Success',
+		isToMerchant: 1,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
+	{
+		transactionHash: '0x10003000000000',
+		amount: 30,
+		from: '0x10000000000000',
+		to: '0x10000000000000',
+		fromEmail: 'from@email.com',
+		toEmail: 'to@email.com',
+		status: 'Success',
+		isToMerchant: 1,
+		confirmedAt: 0,
+		createdAt: 0,
+	},
 ];
