7c1d77332ccace0da9776de7eaab7288525b0879
Esraa <jbesraa@gmail.com>
2021-07-29 11:08:58 +0300

    Update index.ts

--- web-admin/src/components/index.ts
+++ web-admin/src/components/index.ts
@@ -1,6 +1,6 @@
 import * as Inputs from './inputs';
 import Modal from './modal';
 import Sidebar from './Sidebar';
-import TableTemplate from './table';
+import TableTemplate from './Table';
 
 export { Sidebar, TableTemplate, Modal, Inputs };
