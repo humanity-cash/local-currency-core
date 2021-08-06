864d81d6b0470d4ccd2c86d1bc68d4a80182b78a
Esraa Jbara <jbesraa@gmail.com>
2021-08-06 11:00:18 +0300

    small fixes

--- web-admin/src/router/index.tsx
+++ web-admin/src/router/index.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "components";
 import { Route, Switch } from "react-router-dom";
-import { ChangePasswordScreen, ContractsScreen, DashboardScreen, LoginScreen, TransactionsScreen } from "screens";
+import { ChangePasswordScreen, ContractsScreen, DashboardScreen, LoginScreen, TransactionScreen, TransactionsScreen, UsersScreen } from "screens";
 
 export const ProtectedRoutes = () => {
 	return (
@@ -9,7 +9,8 @@ export const ProtectedRoutes = () => {
 			<Switch>
 					<Route path="/dashboard" exact component={() => <DashboardScreen />}/>
 					<Route path="/contracts" exact component={() => <ContractsScreen />}/>
-					<Route path="/users" exact component={() => <ContractsScreen />}/>
+					<Route path="/users" exact component={() => <UsersScreen />}/>
+					<Route path="/transaction/id" exact component={() => <TransactionScreen />}/>
 					<Route path="/ach/transactions" exact component={() => <TransactionsScreen.ACHTransactionsTable />}/>
 					<Route path="/bc/transactions" exact component={() => <TransactionsScreen.BlockchainTransactionsTable />}/>
 			</Switch>
