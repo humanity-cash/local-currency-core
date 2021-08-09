f377d57810ba4a1cd37bb9bfd4f2b50f93b2193c
Aaron Boyd <aaronmboyd@gmail.com>
2021-08-09 14:36:56 +0200

    [FIX] - Remove unneeded validators

--- api/src/router/user/validators.ts
+++ api/src/router/user/validators.ts
@@ -3,18 +3,9 @@ import { mwVaildator } from "src/middlewares";
 
 const idInParams = [param("id").notEmpty(), mwVaildator];
 
-export const createUserSettlement = [
-  body("userId").isString(),
-  body("transactionId").isString(),
-  body("settlementAmount").isNumeric(),
-  mwVaildator,
-];
-
 export const createUser = [body("userId").isString(), mwVaildator];
 
 export const getUser = [...idInParams];
-
-export const getUserAuthorizations = [...idInParams];
 
 export const deposit = [...idInParams, body("amount").isString(), mwVaildator];
 
