e85a90f1cc47df1a6bdbcaad59b5da492ae651d8
Aaron Boyd <aaronmboyd@gmail.com>
2021-08-09 14:35:03 +0200

    [FEAT] - Add timestamps to deposits/withdrawals

--- api/src/utils/crypto.ts
+++ api/src/utils/crypto.ts
@@ -1,6 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as utils from "web3-utils";
+import { BlockTransactionString } from "web3-eth";
+import { getProvider } from "./getProvider";
 
 export function toBytes32(input: string): string {
   return utils.keccak256(input);
+}
+
+export async function getTimestampForBlock(
+  blockNumber: number
+): Promise<string | number> {
+  const provider = await getProvider();
+  const block: BlockTransactionString = await provider.web3.eth.getBlock(
+    blockNumber
+  );
+  return block.timestamp;
 }
