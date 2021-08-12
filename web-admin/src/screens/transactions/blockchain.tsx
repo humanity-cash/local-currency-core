ce7ddfee9838c0539278b4bde5dd47b84605ea8e
Esraa Jbara <jbesraa@gmail.com>
2021-08-12 11:32:01 +0300

    styling and columns restructring

--- web-admin/src/screens/transactions/blockchain.tsx
+++ web-admin/src/screens/transactions/blockchain.tsx
@@ -1,6 +1,7 @@
 import { TableTemplate } from 'components';
 import { useBlockchainData } from 'hooks';
 import moment from 'moment';
+import { useHistory } from 'react-router-dom';
 import { BlockchainData, BlockchainDataState } from 'types';
 import { iconStatus } from 'utils';
 
@@ -10,41 +11,72 @@ interface Column {
   minWidth?: number;
   align?: 'right';
   format?: (value: any) => any;
+  onClick?: (value: any) => any;
+  clickable?: boolean;
 }
 
-const columns: Column[] = [
-  { id: 'transactionHash', label: 'Hash', minWidth: 100 },
-  { id: 'fromUser', label: 'From', minWidth: 100 },
-  { id: 'toUser', label: 'To', minWidth: 100 },
-  { id: 'isToMerchant', label: 'Purchase', minWidth: 100, format: (value: boolean) => value ? 'Yes' : 'No' },
-  {
-    id: 'amount',
-    label: 'Amount',
-    minWidth: 100,
-    format: (value: number) => value.toLocaleString('en-US') + ' B$',
-  },
-  {
-    id: 'createdAt',
-    label: 'Created At',
-    minWidth: 100,
-    format: (value: number) => moment().format(),
-  },
-  {
-    id: 'confirmedAt',
-    label: 'Confirmed At',
-    minWidth: 100,
-    format: (value: number) => moment().format(),
-  },
-  {
-    id: 'status',
-    label: 'Status',
-    minWidth: 100,
-    format: (value: string) => iconStatus(value)
-  },
-];
+const useColumns = () => {
+	const history = useHistory();
+	const columns: Column[] = [
+		{
+			id: 'transactionHash',
+			label: 'Hash',
+			minWidth: 100,
+			clickable: true,
+			onClick: (value: string) => history.push(`/transaction/bc/${value}`),
+		},
+		{
+			id: 'fromUser',
+			label: 'From',
+			minWidth: 100,
+			clickable: true,
+			onClick: (value: string) => history.push(`/user/${value}`),
+		},
+		{
+			id: 'toUser',
+			label: 'To',
+			minWidth: 100,
+			clickable: true,
+			onClick: (value: string) => history.push(`/user/${value}`),
+		},
+		{
+			id: 'isToMerchant',
+			label: 'Purchase',
+			minWidth: 100,
+			format: (value: boolean) => (value ? 'Yes' : 'No'),
+		},
+		{
+			id: 'amount',
+			label: 'Amount',
+			minWidth: 100,
+			format: (value: number) => value.toLocaleString('en-US') + ' B$',
+		},
+		{
+			id: 'createdAt',
+			label: 'Created At',
+			minWidth: 100,
+			format: (value: number) => moment().format(),
+		},
+		{
+			id: 'confirmedAt',
+			label: 'Confirmed At',
+			minWidth: 100,
+			format: (value: number) => moment().format(),
+		},
+		{
+			id: 'status',
+			label: 'Status',
+			minWidth: 100,
+			format: (value: string) => iconStatus(value),
+		},
+	];
+
+	return columns;
+};
 
 
 const BlockchainDataTable = () => {
+	const columns: Column[] = useColumns();
 	const state: BlockchainDataState = useBlockchainData();
 
   return (
