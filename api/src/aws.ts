1ffb0e0d169556050f3e45fe116f684e2c2a8acb
Esraa Jbara <jbesraa@gmail.com>
2021-08-12 14:57:03 +0300

    fix racing

--- api/src/aws.ts
+++ api/src/aws.ts
@@ -3,7 +3,7 @@ import { errors, verifierFactory } from '@southlane/cognito-jwt-verifier';
 
 const { JwtVerificationError, JwksNoMatchingKeyError } = errors;
 
-export const cognitoVerifier = verifierFactory({
+export const cognitoVerifier = () => verifierFactory({
   region: process.env.AWS_REGION,
   userPoolId: process.env.AWS_POOL_ID,
   appClientId: process.env.AWS_CLIENT_ID,
@@ -12,7 +12,8 @@ export const cognitoVerifier = verifierFactory({
 
 export const verifyCognitoToken = async (token: string): Promise<{ success: boolean, token: any }> => {
   try {
-    const verifiedToken = await cognitoVerifier.verify(token);
+		const verifier = cognitoVerifier().verify
+    const verifiedToken = await verifier(token);
 
     return { success: true, token: verifiedToken };
   } catch (e) {
