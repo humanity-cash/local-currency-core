d0fdead433eef9885c3e6bf1bcd7733e635d0a9f
Aaron Boyd <aaronmboyd@gmail.com>
2021-08-19 17:21:59 +0200

    [STYLE] - Prettier

--- api/src/router/user/controller.ts
+++ api/src/router/user/controller.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from "express";
 import * as OperatorService from "src/service/OperatorService";
 import * as PublicServices from "src/service/PublicService";
-import { IDeposit, INewUser, ITransferEvent, IWallet, IWithdrawal } from "src/types";
+import {
+  IDeposit,
+  INewUser,
+  ITransferEvent,
+  IWallet,
+  IWithdrawal,
+} from "src/types";
 import { httpUtils } from "src/utils";
 
 const codes = httpUtils.codes;
 
 export async function getAllUsers(_req: Request, res: Response): Promise<void> {
   try {
-    const users : IWallet[] = await PublicServices.getAllWallets();
+    const users: IWallet[] = await PublicServices.getAllWallets();
     httpUtils.createHttpResponse(users, codes.OK, res);
   } catch (err) {
     httpUtils.createHttpResponse(
@@ -24,7 +30,7 @@ export async function getAllUsers(_req: Request, res: Response): Promise<void> {
 export async function getUser(req: Request, res: Response): Promise<void> {
   try {
     const id = req?.params?.id;
-    const user : IWallet = await PublicServices.getWallet(id);
+    const user: IWallet = await PublicServices.getWallet(id);
     // Create as an array of one item for API consistency
     httpUtils.createHttpResponse([user], codes.OK, res);
   } catch (err) {
@@ -50,9 +56,9 @@ export async function getUser(req: Request, res: Response): Promise<void> {
 
 export async function createUser(req: Request, res: Response): Promise<void> {
   try {
-    const newUser : INewUser = req.body;
+    const newUser: INewUser = req.body;
     await OperatorService.createUser(newUser);
-    const wallet : IWallet = await PublicServices.getWallet(newUser.userId);
+    const wallet: IWallet = await PublicServices.getWallet(newUser.userId);
     httpUtils.createHttpResponse(wallet, codes.CREATED, res);
   } catch (err) {
     if (err.message?.includes("ERR_USER_EXISTS"))
@@ -79,7 +85,7 @@ export async function deposit(req: Request, res: Response): Promise<void> {
     const id = req?.params?.id;
     const deposit = req.body;
     await OperatorService.deposit(id, deposit.amount);
-    const wallet : IWallet = await PublicServices.getWallet(id);
+    const wallet: IWallet = await PublicServices.getWallet(id);
     httpUtils.createHttpResponse(wallet, codes.ACCEPTED, res);
   } catch (err) {
     if (err?.message?.includes("ERR_USER_NOT_EXIST"))
@@ -122,7 +128,7 @@ export async function getDeposits(req: Request, res: Response): Promise<void> {
   try {
     const id = req?.params?.id;
     await PublicServices.getWallet(id);
-    const deposits : IDeposit[] = await OperatorService.getDepositsForUser(id);
+    const deposits: IDeposit[] = await OperatorService.getDepositsForUser(id);
     httpUtils.createHttpResponse(deposits, codes.OK, res);
   } catch (err) {
     if (err?.message?.includes("ERR_USER_NOT_EXIST"))
@@ -151,7 +157,8 @@ export async function getWithdrawals(
   try {
     const id = req?.params?.id;
     await PublicServices.getWallet(id);
-    const withdrawals : IWithdrawal[] = await OperatorService.getWithdrawalsForUser(id);
+    const withdrawals: IWithdrawal[] =
+      await OperatorService.getWithdrawalsForUser(id);
     httpUtils.createHttpResponse(withdrawals, codes.OK, res);
   } catch (err) {
     if (err?.message?.includes("ERR_USER_NOT_EXIST"))
@@ -176,7 +183,8 @@ export async function getWithdrawals(
 export async function getTransfers(req: Request, res: Response): Promise<void> {
   try {
     const id = req?.params?.id;
-    const transfers : ITransferEvent[] = await OperatorService.getTransfersForUser(id);
+    const transfers: ITransferEvent[] =
+      await OperatorService.getTransfersForUser(id);
     httpUtils.createHttpResponse(transfers, codes.OK, res);
   } catch (err) {
     if (err?.message?.includes("ERR_USER_NOT_EXIST"))
@@ -203,7 +211,7 @@ export async function withdraw(req: Request, res: Response): Promise<void> {
     const id = req?.params?.id;
     const withdrawal = req.body;
     await OperatorService.withdraw(id, withdrawal.amount);
-    const wallet : IWallet = await PublicServices.getWallet(id);
+    const wallet: IWallet = await PublicServices.getWallet(id);
     httpUtils.createHttpResponse(wallet, codes.ACCEPTED, res);
   } catch (err) {
     if (err?.message?.includes("ERR_USER_NOT_EXIST"))
@@ -255,7 +263,7 @@ export async function transferTo(req: Request, res: Response): Promise<void> {
     const id = req?.params?.id;
     const transfer = req.body;
     await OperatorService.transferTo(id, transfer.toUserId, transfer.amount);
-    const user : IWallet = await PublicServices.getWallet(id);
+    const user: IWallet = await PublicServices.getWallet(id);
     httpUtils.createHttpResponse(user, codes.ACCEPTED, res);
   } catch (err) {
     if (err?.message?.includes("ERR_USER_NOT_EXIST"))
