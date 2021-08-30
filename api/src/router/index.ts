73d11fe2b7b37a1190347acc4b1685eea13fa004
Aaron Boyd <aaronmboyd@gmail.com>
2021-08-31 01:03:54 +0200

    [FEAT] - Add deposit, withdrawal, transfer, and operator statistics
    Fixes #17
    Fixes #18

--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import user from "./user";
+import webhook from "./webhook";
+import stats from "./stats";
 
 const router = express();
 
+router.use(webhook);
 router.use(user);
+router.use(stats);
 
 export default router;
