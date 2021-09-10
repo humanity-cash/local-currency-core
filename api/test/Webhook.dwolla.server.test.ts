f7c939580767ba2a975699563b7b1de6eb6e1a66
Aaron Boyd <aaronmboyd@gmail.com>
2021-09-10 13:30:54 +0200

    [TEST] - Remove explicit dotenv usage in favour of Jest config
    Also fix tests to use resourceUri instead of id

--- api/test/Webhook.dwolla.server.test.ts
+++ api/test/Webhook.dwolla.server.test.ts
@@ -1,6 +1,4 @@
-import dotenv from "dotenv";
 import chai from "chai";
-import path from "path";
 import chaiHttp from "chai-http";
 import { getApp } from "../src/server";
 import { setupContracts, getSalt, createDummyEvent } from "./utils";
@@ -26,13 +24,6 @@ import faker from "faker";
 const expect = chai.expect;
 chai.use(chaiHttp);
 const server = getApp();
-
-const result = dotenv.config({
-  path: path.resolve(process.cwd(), ".env.test"),
-});
-if (result.error) {
-  throw result.error;
-} else log(result);
 
 const CUSTOMERS_TO_CREATE = 1;
 
