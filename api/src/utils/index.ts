42d138efca22b6269720f36dcce04e33de7bd747
Aaron Boyd <aaronmboyd@gmail.com>
2021-10-13 15:38:52 +0200

    [FEAT] - Log messages to database (commented out for now)

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,11 +1,21 @@
 import * as cryptoUtils from "./crypto";
 import * as httpUtils from "./http";
+// import { LogService } from "src/database/service";
 
 export { cryptoUtils, httpUtils };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function log(...data: any[]): void {
   if (process.env.DEBUG == "true") console.log(...data);
+  // const logItem : LogService.ICreateLogDBItem = {
+  //   timestamp: Date.now(),
+  //   message: data.join(" ")
+  // }
+  // LogService.create(logItem)
+  // .then(res => {
+  //   if (process.env.DEBUG == "true")
+  //     console.log("log() LogService: Log item created with dbId " + JSON.stringify(res, null, 2))
+  // });
 }
 
 export function isDevelopment(): boolean {
