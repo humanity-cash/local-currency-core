1c5cdc7c9e3dee2c0d66c8d95cfa0c94ce3eea4d
Esraa Jbara <jbesraa@gmail.com>
2021-10-28 18:40:01 +0300

    add validators

--- api/src/router/user/validators.ts
+++ api/src/router/user/validators.ts
@@ -40,6 +40,48 @@ export const getUser = [...idInParams];
 
 export const deposit = [...idInParams, body("amount").isString(), mwVaildator];
 
+export const addCustomerVerification = [
+  ...idInParams, 
+  body("business.dowllaId").isString(),
+  body("customer.tag").isString(),
+  body("customer.avatar").isString(),
+  body("customer.address1").isString(),
+  body("customer.address2").isString(),
+  body("customer.city").isString(),
+  body("customer.state").isString(),
+  body("customer.postalCode").isString(),
+  body("customer.firstName").isString(),
+  body("customer.lastName").isString(),
+  mwVaildator
+];
+
+export const addBusinessVerification = [
+  ...idInParams, 
+  body("customer.dowllaId").isString(),
+  body("business.story").isString(),
+  body("business.tag").isString(),
+  body("business.avatar").isString(),
+  body("business.type").isString(),
+  body("business.rbn").isString(),
+  body("business.industry").isString(),
+  body("business.ein").isString(),
+  body("business.address1").isString(),
+  body("business.address2").isString(),
+  body("business.city").isString(),
+  body("business.state").isString(),
+  body("business.postalCode").isString(),
+  body("business.phoneNumber").isString(),
+  body("business.owner.firstName").isString(),
+  body("business.owner.lastName").isString(),
+  body("business.owner.email").isString(),
+  body("business.owner.address1").isString(),
+  body("business.owner.address2").isString(),
+  body("business.owner.city").isString(),
+  body("business.owner.state").isString(),
+  body("business.owner.postalCode").isString(),
+  mwVaildator
+];
+
 export const withdraw = [...idInParams, body("amount").isString(), mwVaildator];
 
 export const transfer = [
