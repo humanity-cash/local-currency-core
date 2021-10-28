ccb6c131b52b52d2bbea4982d630b8c842893d27
Esraa Jbara <jbesraa@gmail.com>
2021-10-28 18:41:27 +0300

    add auth service

--- api/src/service/AuthService.ts
+++ api/src/service/AuthService.ts
@@ -1,21 +1,63 @@
-import { BusinessModel, CustomerModel } from "src/database/schema/User";
 import { UserService as UserDatabaseService } from "src/database/service";
+import { DowllaId, IAddBusinessVerification, IAddCustomerVerification, IBusinessDowllaId, ICustomerDowllaId, INewBusinessDocument, INewCustomerDocument, User } from "src/types";
+import { log } from "src/utils";
 
-export async function createUser({ email, consent }) {
-	const user = await UserDatabaseService.create({ email, consent });
+type GenericDatabaseResponse<T, E = string> = { success: boolean, data?: T, error?: E };
 
-	return user;
+export async function createCustomer(input: Pick<User, 'email' | 'consent' | 'customer'>)
+	: Promise<GenericDatabaseResponse<INewCustomerDocument>> {
+	try {
+		if (!input?.email || !input?.customer || !input?.consent) return {
+			success: false, error: "Invalid Inputs"
+		};
+		const user: INewCustomerDocument = await UserDatabaseService.create<INewCustomerDocument>({
+			consent: input.consent,
+			verifiedCustomer: true,
+			verifiedBusiness: false,
+			customer: input.customer,
+			email: input.email,
+		});
+		delete user.customer._id;
+		return { success: true, data: user };
+	} catch (error) {
+		log(error);
+		return { success: false, error: "Something went wrong!" };
+	}
+};
+
+export async function createBusiness(input: Pick<User, 'email' | 'consent' | 'business'>)
+	: Promise<GenericDatabaseResponse<INewBusinessDocument>> {
+	try {
+		if (!input?.email || !input?.business || !input?.consent) return {
+			success: false, error: "Invalid Inputs"
+		};
+		const user = await UserDatabaseService.create<INewBusinessDocument>({
+			consent: input.consent,
+			verifiedCustomer: false,
+			verifiedBusiness: true,
+			business: input.business,
+			email: input.email,
+		});
+		delete user.business._id;
+		return { success: true, data: user };
+	} catch (error) {
+		log(error);
+		return { success: false, error: "Something went wrong!" };
+	}
+};
+
+export async function addCustomerVerification(dowllaId: DowllaId,
+	verification: IAddCustomerVerification): Promise<any> {
+	const filter: IBusinessDowllaId = { 'business': { 'dowllaId': dowllaId } };
+	const response = await UserDatabaseService.update(filter,
+		{ ...verification, verifiedCustomer: true });
+	return response;
 }
 
-export async function verifyCustomer({ email, customer }) {
-	const customerData = new CustomerModel({ ...customer })
-	const user = await UserDatabaseService.update(email, { customer: customerData, verifiedCustomer: true });
-
-	return user;
-}
-
-export async function verifyBusiness({ email, business }) {
-	const businessData = new BusinessModel({ ...business })
-	const user = await UserDatabaseService.update(email, { business: businessData, verifiedBusiness: true });
-	return user;
+export async function addBusinessVerification(dowllaId: DowllaId,
+	verification: IAddBusinessVerification): Promise<any> {
+	const filter: ICustomerDowllaId = { 'customer': { 'dowllaId': dowllaId } };
+	const response = await UserDatabaseService.update(filter,
+		{ ...verification, verifiedBusiness: true });
+	return response;
 }
