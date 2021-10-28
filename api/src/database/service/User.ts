a03d1eb04a2e196b9f4d67faef40e5f64f43a77c
Esraa Jbara <jbesraa@gmail.com>
2021-10-28 18:41:20 +0300

    add basic create update get

--- api/src/database/service/User.ts
+++ api/src/database/service/User.ts
@@ -1,20 +1,21 @@
-import { User } from "src/types";
+import { IBusinessDowllaId, ICustomerDowllaId, UpdateUser, User } from "src/types";
 import { User as UserSchema } from "../schema";
 import { removeMongoMeta } from "../utils/index";
 
-export async function create(input: Omit<User, 'verifiedCustomer' | 'verifiedBusiness'>): Promise<any> {
-		const user = new UserSchema({
-			consent: input.consent,
-			verifiedCustomer: false,
-			verifiedBusiness: false,
-			email: input.email,
-		});
-		const response = await user.save();
-		return  removeMongoMeta(response.toObject());
+
+export async function create<T>(input: User): Promise<T> {
+	const user = new UserSchema({
+		...input
+	});
+	const response = await user.save();
+
+	return removeMongoMeta(response.toObject());
 };
 
-export async function update(email: string, update: any): Promise<any> {
-	const response = await UserSchema.findOneAndUpdate({ email }, update, { upsert: false, new: true });
+type UserFilter = IBusinessDowllaId | ICustomerDowllaId;
+
+export async function update(filter: UserFilter, update: UpdateUser): Promise<any> {
+	const response = await UserSchema.findOneAndUpdate(filter, update, { upsert: false, new: true });
 	return removeMongoMeta(response.toObject());
 }
 
