b683c6c0f09f54096b8a45942f44594942028593
Esraa Jbara <jbesraa@gmail.com>
2021-10-29 11:45:31 +0300

    test add verifications to existing users

--- api/src/database/service/User.ts
+++ api/src/database/service/User.ts
@@ -1,25 +1,23 @@
-import { IBusinessDowllaId, ICustomerDowllaId, UpdateUser, User } from "src/types";
+import { IBusinessDowllaId, ICustomerDowllaId, INewUserInput, UpdateUser } from "src/types";
 import { User as UserSchema } from "../schema";
 import { removeMongoMeta } from "../utils/index";
 
-
-export async function create<T>(input: User): Promise<T> {
+export async function create<T>(input: INewUserInput): Promise<T> {
 	const user = new UserSchema({
 		...input
 	});
 	const response = await user.save();
-
-	return removeMongoMeta(response.toObject());
+	return removeMongoMeta(response?.toObject());
 };
 
 type UserFilter = IBusinessDowllaId | ICustomerDowllaId;
 
-export async function update(filter: UserFilter, update: UpdateUser): Promise<any> {
+export async function update<T>(filter: UserFilter, update: UpdateUser): Promise<T> {
 	const response = await UserSchema.findOneAndUpdate(filter, update, { upsert: false, new: true });
-	return removeMongoMeta(response.toObject());
+	return removeMongoMeta(response?.toObject());
 }
 
 export async function get(email: string): Promise<any> {
 	const response = await UserSchema.findOne({ email });
-	return removeMongoMeta(response.toObject());
+	return removeMongoMeta(response?.toObject());
 }
