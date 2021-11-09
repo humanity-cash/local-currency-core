eede92d0ca6de06a296c494f507ae2dbfb4e60f8
Aaron Boyd <aaronmboyd@gmail.com>
2021-11-09 11:56:50 +0100

    Linting

--- api/src/database/service/User.ts
+++ api/src/database/service/User.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { ObjectId } from "mongoose";
 import { IBusinessDwollaId, ICustomerDwollaId, INewUserInput } from "src/types";
 import { User as UserSchema } from "../schema";
