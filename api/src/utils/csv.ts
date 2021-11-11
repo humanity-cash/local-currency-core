fc7a2e253e9566278f61d47b763075379dd8ba78
Esraa Jbara <jbesraa@gmail.com>
2021-11-11 15:23:21 +0200

    add types

--- api/src/utils/csv.ts
+++ api/src/utils/csv.ts
@@ -1,12 +1,12 @@
-export function convertArrayToCSV(array) {
+export function convertArrayToCSV(input: any[]): string {
   // Use first element to choose the keys and the order
-  const keys = Object.keys(array[0]);
+  const keys = Object.keys(input[0]);
 
   // Build header
   let result = keys.join(",") + "\n";
 
   // Add the rows
-  array.forEach(function (obj) {
+  input.forEach(function (obj) {
     result += keys.map((k) => obj[k]).join(",") + "\n";
   });
 
