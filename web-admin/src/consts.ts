7490b26c5338f38daa359b63e4affa68d1f5dcad
Esraa Jbara <jbesraa@gmail.com>
2021-11-26 15:07:05 +0200

    add new consts and small refactor

--- web-admin/src/consts.ts
+++ web-admin/src/consts.ts
@@ -1,8 +1,12 @@
 
 /**Routes */
-export const START_CONTRACT = '/contract/actions/start'
-export const STOP_CONTRACT = '/contract/actions/start'
-export const GET_CONTRACTS = '/contract/all'
+export const START_CONTRACT = '/admin/unpause'
+export const STOP_CONTRACT = '/admin/pause'
+
+export const STATS_DEPOSITS = '/stats/deposit'
+export const STATS_WITHDRAWAL = '/stats/withdrawal'
+export const STATS_OPERATOR = '/stats/operator'
+export const STATS_TRANSFER = '/stats/transfer'
 
 /**AWS COGNITO ERRORS */
 export const NEW_PASSWORD_REQUIRED_ERROR = 'newPasswordRequiredError'
