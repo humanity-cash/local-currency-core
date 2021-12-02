a458708cf60473cb362beafb9af1517cf4000b27
Aaron Boyd <aaronmboyd@gmail.com>
2021-12-02 16:18:11 +0100

    [FEAT] - Configure infrastructure for Elastic Beanstalk on AWS
    Connect MongodDB to remote hosted DB

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
 import { log } from "src/utils";
 
-// mongoose.connect('mongodb://username:password@host:port/database?options...', {useNewUrlParser: true});
-// cb(callback) input will be executed after database started
 const startDatabase = (cb: () => void): void => {
   const databaseURL = process.env.MONGO_URL;
   if (!databaseURL) {
     log("Databse URL is not set. Aborting.");
-
     return;
   }
   log(`Connecting to databse ${databaseURL}`);
-  mongoose.connect(databaseURL + "?authSource=admin", {
+  mongoose.connect(databaseURL + "&authSource=admin", {
+    auth: {
+      user: process.env.MONGO_DB_USER,
+      password: process.env.MONGO_DB_PASSWORD,
+    },
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    tlsCAFile: `rds-combined-ca-bundle.pem`,
   });
   const db = mongoose.connection;
   db.on("err", (error: unknown) => {
