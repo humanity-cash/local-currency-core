b6d4c7fef1e1d932d1065a2a6fcbc995fa52ae0b
Aaron Boyd <aaronmboyd@gmail.com>
2021-12-09 20:33:26 +0100

    [FIX] - Troubleshooting MongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
 import "./aliases";
 import startDatabase from "./database";
 import { getApp } from "./server";
-import { isDwollaProduction, log, logSettings, shouldRegisterWebhook, shouldSimulateBanking, shouldSimulateWebhook, shouldUseManagedSecrets } from "src/utils";
+import {
+  isDwollaProduction,
+  log,
+  logSettings,
+  shouldRegisterWebhook,
+  shouldSimulateBanking,
+  shouldSimulateWebhook,
+  shouldUseManagedSecrets,
+} from "src/utils";
 import { configureEnvironment } from "./utils/configuration";
 import { registerWebhook } from "./service/digital-banking/DwollaWebhookService";
 import { processDwollaSandboxSimulations } from "./test/utils";
@@ -11,44 +17,49 @@ import { processDwollaSandboxSimulations } from "./test/utils";
 const app = getApp();
 
 const runApp = async () => {
+  logSettings();
 
-	logSettings();
+  if (shouldUseManagedSecrets()) {
+    await configureEnvironment();
+  }
+  if (shouldRegisterWebhook()) {
+    if (shouldSimulateWebhook())
+      throw Error(
+        `Invalid configuration, REGISTER_WEBHOOK and SIMULATE_WEBHOOK cannot both be "true"`
+      );
 
-	if(shouldUseManagedSecrets()){
-		await configureEnvironment();
-	}
-	if(shouldRegisterWebhook()){
-		if(shouldSimulateWebhook())
-			throw Error(`Invalid configuration, REGISTER_WEBHOOK and SIMULATE_WEBHOOK cannot both be "true"`);
+    await registerWebhook();
+  }
+  if (shouldSimulateBanking()) {
+    if (isDwollaProduction())
+      throw Error(
+        `Invalid configuration, SIMULATE_BANKING cannot be used in Dwolla production environment`
+      );
 
-		await registerWebhook();
-	}
-	if(shouldSimulateBanking()){
+    setInterval(async () => {
+      await processDwollaSandboxSimulations();
+    }, 60000);
+  }
 
-		if(isDwollaProduction())
-			throw Error(`Invalid configuration, SIMULATE_BANKING cannot be used in Dwolla production environment`);
-
-		setInterval(async () => {
-			await processDwollaSandboxSimulations();
-		}, 60000);  	
-	}
-	
-	app.listen(process.env.PORT, () => {
-		log(			
-			"NODE_ENV:",
-			process.env.NODE_ENV,
-			"\nLOCAL_CURRENCY_ADDRESS:",
-			process.env.LOCAL_CURRENCY_ADDRESS,
-			"\nLOCAL_CURRENCY_RPC_HOST:",
-			process.env.LOCAL_CURRENCY_RPC_HOST,
-			"\nLOCAL_CURRENCY_MNEMONIC set:",
-			!!process.env.LOCAL_CURRENCY_MNEMONIC,
-			"\nDERIVATION_PATH:",
-			process.env.DERIVATION_PATH
-		);
-		log(`App listening at http://localhost:${process.env.PORT}`);
-		startDatabase(() => {log("App with database started")});		
-	});
-}
+  app.listen(process.env.PORT, () => {
+    log(
+      "NODE_ENV:",
+      process.env.NODE_ENV,
+      "\nLOCAL_CURRENCY_ADDRESS:",
+      process.env.LOCAL_CURRENCY_ADDRESS,
+      "\nLOCAL_CURRENCY_RPC_HOST:",
+      process.env.LOCAL_CURRENCY_RPC_HOST,
+      "\nLOCAL_CURRENCY_MNEMONIC set:",
+      !!process.env.LOCAL_CURRENCY_MNEMONIC,
+      "\nDERIVATION_PATH:",
+      process.env.DERIVATION_PATH
+    );
+    log(`App listening at http://localhost:${process.env.PORT}`);
+    startDatabase((err) => {
+      if (err) throw err;
+      else log("App with database started");
+    });
+  });
+};
 
 runApp();
