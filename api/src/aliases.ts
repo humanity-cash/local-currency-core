4715f67386531d5cc6d8247d3ae73b9a2ab0367e
Aaron Boyd <aaronmboyd@gmail.com>
2021-12-09 15:41:15 +0100

    [STYLE] - Prettier

--- api/src/aliases.ts
+++ api/src/aliases.ts
@@ -1,9 +1,8 @@
-import moduleAlias from 'module-alias';
-import path from 'path';
+import moduleAlias from "module-alias";
+import path from "path";
 
-const rootPath = path.resolve(__dirname, '..');
-const rootPathProd = path.resolve(rootPath, 'dist');
+const rootPath = path.resolve(__dirname, "..");
+const rootPathProd = path.resolve(rootPath, "dist");
 moduleAlias.addAliases({
-  'src':  rootPathProd,
+  src: rootPathProd,
 });
-
--- api/src/server.ts
+++ api/src/server.ts
@@ -14,4 +14,4 @@ export function getApp(): Express {
   app.use(router);
 
   return app;
-};
+}
