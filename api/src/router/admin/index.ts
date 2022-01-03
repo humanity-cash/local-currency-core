084ca54cdc7e3806f37c24499a81feed1c8ac486
Aaron Boyd <aaronmboyd@gmail.com>
2022-01-03 16:29:41 +0100

    [FEAT] - Add back in authentication middlewares
    Fixes #117

--- api/src/router/admin/index.ts
+++ api/src/router/admin/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import * as controller from "./controller";
 import * as validator from "./validators";
+import { verifyRequest } from "src/middlewares";
 
 const admin = express();
-admin.post("/admin/pause", controller.adminPause);
-admin.post("/admin/unpause", controller.adminUnpause);
+admin.post("/admin/pause", verifyRequest, controller.adminPause);
+admin.post("/admin/unpause", verifyRequest, controller.adminUnpause);
 admin.post(
   "/admin/transfer/controller",
   validator.transferOwnerController,
