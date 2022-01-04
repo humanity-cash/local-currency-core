b9d6deb6c4d667dd67a9ddb3d62bc42f351010df
Aaron Boyd <aaronmboyd@gmail.com>
2022-01-04 17:02:57 +0100

    [FEAT] - Content endpoint
    Fixes #84

---
+++ api/src/content/Heroes.ts
@@ -0,0 +1,40 @@
+import { HomeScreenContent } from "../types";
+
+export const Heroes: HomeScreenContent[] = [
+  {
+    contentType: "Heroes",
+    image: "https://berkshares-images.imgix.net/BerkShare_1-front.jpg",
+    textTitle: "The Mohican Nation",
+    text: "Original Native American inhabitants of these lands and waterways prior to European settlement. Their responsible stewardship of our environment recalls our obligation to respect and care for our living ecology. A township was granted to tribe members in 1736 in today’s Stockbridge and West Stockbridge. These lands were later lost to white settlement through mortgage, debt, and sometimes fraud, and the group eventually accepted an offer of removal, and today’s Stockbridge-Munsee Band is centered around a reservation in Wisconsin. They still look to the Berkshires as a part of their ancestral homeland.",
+  },
+  {
+    contentType: "Heroes",
+    image: "https://berkshares-images.imgix.net/BerkShare_5-front.jpg",
+    textTitle: "W. E. B. Du Bois",
+    text: "Sociologist and civil rights pioneer committed to the freedom and equality of all peoples. Born in Great Barrington in 1868, Du Bois’ activities contributed to the formation of the National Association for the Advancement of Colored People. For twenty-five years he served as editor of the NAACP Magazine, The Crisis. Today, Du Bois’s boyhood homesite in the Berkshires is dedicated as a National Historic Landmark.",
+  },
+  {
+    contentType: "Heroes",
+    image: "https://berkshares-images.imgix.net/BerkShare_50-front.jpg",
+    textTitle: "Norman Rockwell",
+    text: "Iconic 20th century American artist celebrated for his  vivid and affectionate portraits of everyday life. The people and community of Stockbridge are eternally woven into Rockwell’s classic depictions. The Norman Rockwell Museum at Stockbridge is a trust dedicated to his legacy.",
+  },
+  {
+    contentType: "Heroes",
+    image: "https://berkshares-images.imgix.net/BerkShare_20-front.jpg",
+    textTitle: "Herman Melville",
+    text: "Writer and mariner lauded for his great American novel, Moby-Dick written at Arrowhead. Melville frequented the Berkshires as a boy and, inspired by the beauty of the area, settled near Pittsfield in 1850. Written at Arrowhead farmhouse by 1851, its brilliance — with timeless reflections on human nature and environmental ethics — was not fully recognized until later. Arrowhead  is today preserved as a National Historic Landmark.",
+  },
+  {
+    contentType: "Heroes",
+    image: "https://berkshares-images.imgix.net/BerkShare_10-front.jpg",
+    links: [
+      {
+        url: "https://berkshiregrown.org/",
+        text: "Berkshire Grown",
+      },
+    ],
+    textTitle: "Robyn Van En",
+    text: "A pioneer of the community supported agriculture (CSA) movement in North America. Van En co-founded the forerunner to today’s Berkshire Grown. Her Indian Line Farm in South Egremont has been a model for small-scale, sustainable agriculture since 1985. Her work lives on locally and in more than 3,000 CSAs now active globally.",
+  },
+];
