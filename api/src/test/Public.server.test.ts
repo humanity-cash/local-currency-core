c05e2ff61b9a8cac028d1d64ebce33273db03c2f
Aaron Boyd <aaronmboyd@gmail.com>
2022-01-10 12:39:34 +0100

    [FEAT] - Added `random` `limit` and `type` query parameters
    Added /content unit tests

--- api/src/test/Public.server.test.ts
+++ api/src/test/Public.server.test.ts
@@ -1,8 +1,6 @@
 import { beforeAll, describe, it } from "@jest/globals";
 import chai from "chai";
 import chaiHttp from "chai-http";
-// import * as sinon from "sinon";
-// import * as aws from "../aws";
 import { getApp } from "../server";
 import { log } from "../utils";
 import { codes } from "../utils/http";
@@ -30,13 +28,9 @@ describe("Public endpoints test", () => {
     });
 
     it("it should retrieve heath data", (done) => {
-      // const stub = sinon
-      //   .stub(aws, "verifyCognitoToken")
-      //   .returns({ success: true });
       chai
         .request(server)
         .get("/health")
-        .set("authorization", "tokeeeen")
         .then((res) => {
           expect(res).to.have.status(codes.OK);
           log(JSON.parse(res.text));
@@ -48,7 +42,112 @@ describe("Public endpoints test", () => {
           expect(res.body).to.haveOwnProperty("owner");
           expect(res.body).to.haveOwnProperty("walletFactory");
           expect(res.body).to.haveOwnProperty("controllerStatus");
-          // expect(stub.calledOnce).to.eql(true);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  });
+
+  describe("GET /content", () => {
+    it("it should retrieve content data", (done) => {
+      chai
+        .request(server)
+        .get("/content")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          log(JSON.parse(res.text));
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it("it should retrieve 6 pieces of random content data ", (done) => {
+      chai
+        .request(server)
+        .get("/content?random=6")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          expect(res.body.length).to.equal(6);
+          log(JSON.parse(res.text));
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it("it should retrieve a limit of 3 pieces of content data ", (done) => {
+      chai
+        .request(server)
+        .get("/content?limit=3")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          expect(res.body.length).to.equal(3);
+          log(JSON.parse(res.text));
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it("it should retrieve a limited set of 6 content pieces from 10 random content data ", (done) => {
+      chai
+        .request(server)
+        .get("/content?random=10&limit=6")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          expect(res.body.length).to.equal(6);
+          log(JSON.parse(res.text));
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it("it should retrieve only Heroes type content", (done) => {
+      chai
+        .request(server)
+        .get("/content?type=Heroes")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          expect(res.body.length).to.equal(5);
+          log(JSON.parse(res.text));
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it("it should retrieve only Heroes type content but in random order", (done) => {
+      chai
+        .request(server)
+        .get("/content?type=Heroes&random=5")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          expect(res.body.length).to.equal(5);
+          log(JSON.parse(res.text));
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it("it should retrieve only 3 Featured Artists type content", (done) => {
+      chai
+        .request(server)
+        .get("/content?type=FeaturedArtists&limit=3")
+        .then((res) => {
+          expect(res).to.have.status(codes.OK);
+          expect(res.body.length).to.equal(3);
+          log(JSON.parse(res.text));
           done();
         })
         .catch((err) => {
