fc8f5d26835c84d7ea6127bd99c2d97608ec7e89
Esraa Jbara <jbesraa@gmail.com>
2022-01-11 12:10:24 +0200

    return verifyRequest

--- api/src/router/businesses/index.ts
+++ api/src/router/businesses/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import * as controller from "./controller";
+import { verifyRequest } from "src/middlewares";
 
 const businesses = express();
 
-businesses.get("/businesses", controller.getAllBusinesses);
+businesses.get("/businesses", verifyRequest, controller.getAllBusinesses);
 
 export default businesses;
