16fd41082a8da7985d91b3513368d354d7fc5976
Esraa Jbara <jbesraa@gmail.com>
2022-01-11 11:21:21 +0200

    remove verifyRequest

--- api/src/router/admin/index.ts
+++ api/src/router/admin/index.ts
@@ -1,20 +1,17 @@
 import express from "express";
 import * as controller from "./controller";
 import * as validator from "./validators";
-import { verifyRequest } from "src/middlewares";
 
 const admin = express();
-admin.post("/admin/pause", verifyRequest, controller.adminPause);
-admin.post("/admin/unpause", verifyRequest, controller.adminUnpause);
+admin.post("/admin/pause", controller.adminPause);
+admin.post("/admin/unpause", controller.adminUnpause);
 admin.post(
   "/admin/transfer/controller",
-  verifyRequest,
   validator.transferOwnerController,
   controller.transferControllerOwner
 );
 admin.post(
   "/admin/transfer/user",
-  verifyRequest,
   validator.transferOwnerUser,
   controller.transferWalletOwner
 );
