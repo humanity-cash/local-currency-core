f26127a75c383316bd1da79da8b0aa50aad4e3af
Esraa Jbara <jbesraa@gmail.com>
2022-01-11 11:22:59 +0200

    prettier

--- api/src/router/user/index.ts
+++ api/src/router/user/index.ts
@@ -6,44 +6,24 @@ const user = express();
 
 // Get and create user(s)
 user.get("/users", controller.getAllUsers);
-user.post(
-  "/users",
-  validators.createUser,
-  controller.createUser
-);
+user.post("/users", validators.createUser, controller.createUser);
 user.get("/users/:id", validators.getUser, controller.getUser); // User dwolla info
 user.get("/users/email/:email", controller.getUserByEmail); // User database info
 
 // Get and create deposit(s) for a user
 user.get("/users/:id/deposit", validators.getUser, controller.getDeposits);
-user.post(
-  "/users/:id/deposit",
-  validators.deposit,
-  controller.deposit
-);
+user.post("/users/:id/deposit", validators.deposit, controller.deposit);
 
 // Get and create withdrawal(s) for a user
 user.get("/users/:id/withdraw", validators.getUser, controller.getWithdrawals);
-user.post(
-  "/users/:id/withdraw",
-  validators.withdraw,
-  controller.withdraw
-);
+user.post("/users/:id/withdraw", validators.withdraw, controller.withdraw);
 
 // Get and create transfer(s) for a user
 user.get("/users/:id/transfer", validators.getUser, controller.getTransfers);
-user.post(
-  "/users/:id/transfer",
-  validators.transfer,
-  controller.transferTo
-);
+user.post("/users/:id/transfer", validators.transfer, controller.transferTo);
 
 // Get Dwolla iav-token for a user (via POST)
-user.post(
-  "/users/:id/iav-token",
-  validators.getUser,
-  controller.getIAVToken
-);
+user.post("/users/:id/iav-token", validators.getUser, controller.getIAVToken);
 
 // Get funding sources for a user
 user.get(
