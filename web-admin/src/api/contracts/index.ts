a30f8a8dd47442bd8e7652ab46e9816d19a232b0
Aaron Boyd <aaronmboyd@gmail.com>
2022-01-12 16:18:06 +0100

    [STYLE] - Prettier

--- web-admin/src/api/contracts/index.ts
+++ web-admin/src/api/contracts/index.ts
@@ -1,37 +1,37 @@
-import { START_CONTRACT, STATS_TRANSFER, STOP_CONTRACT } from 'consts';
-import * as BaseAPI from '../base';
-import { formatTransfers } from '../../formatters/index';
-import { IBlockchainTransaction } from '../../types';
+import { START_CONTRACT, STATS_TRANSFER, STOP_CONTRACT } from "consts";
+import * as BaseAPI from "../base";
+import { formatTransfers } from "../../formatters/index";
+import { IBlockchainTransaction } from "../../types";
 
 type Hash = string;
 
 export const getAllTransfers = async (): Promise<IBlockchainTransaction[]> => {
-	try {
-		const response = await BaseAPI.getRequest(STATS_TRANSFER);
+  try {
+    const response = await BaseAPI.getRequest(STATS_TRANSFER);
 
-		return formatTransfers(response);
-	}	catch(err) {
-		console.log('err', err);
-		return []
-	}
-}
+    return formatTransfers(response);
+  } catch (err) {
+    console.log("err", err);
+    return [];
+  }
+};
 
 export const startContract = async (hash: Hash) => {
-	try {
-		const response = await BaseAPI.postRequest(START_CONTRACT, { hash });
+  try {
+    const response = await BaseAPI.postRequest(START_CONTRACT, { hash });
 
-		return response;
-	}	catch(err) {
-		console.log('err', err);
-	}
-}
+    return response;
+  } catch (err) {
+    console.log("err", err);
+  }
+};
 
 export const stopContract = async (hash: Hash) => {
-	try {
-		const response = await BaseAPI.postRequest(STOP_CONTRACT, { hash });
+  try {
+    const response = await BaseAPI.postRequest(STOP_CONTRACT, { hash });
 
-		return response;
-	}	catch(err) {
-		console.log('err', err);
-	}
-}
+    return response;
+  } catch (err) {
+    console.log("err", err);
+  }
+};
