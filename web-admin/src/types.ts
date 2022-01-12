98e8345a7d8c00f4d0e5c6133cbafe8c5608b51e
Aaron Boyd <aaronmboyd@gmail.com>
2022-01-12 16:18:06 +0100

    [STYLE] - Prettier

--- web-admin/src/types.ts
+++ web-admin/src/types.ts
@@ -1,98 +1,98 @@
-import { AxiosResponse } from 'axios';
+import { AxiosResponse } from "axios";
 
 export type AxiosPromiseResponse<T = unknown> = AxiosResponse<T>;
 
 export interface ContractData {
-	name: string;
-	address: string;
-	deployedAt: number;
-	status: number | undefined;
-	version: number;
+  name: string;
+  address: string;
+  deployedAt: number;
+  status: number | undefined;
+  version: number;
 }
 
 export interface ContractsState {
-	data: ContractData[]
+  data: ContractData[];
 }
 
 export interface ModalState {
-	type: string;
-	isOpen: boolean;
-	modalProps: {};
+  type: string;
+  isOpen: boolean;
+  modalProps: {};
 }
 
 export interface BlockchainDataState {
-	data: BlockchainData[]
+  data: BlockchainData[];
 }
 
-type TransactionStatus = 'Pending' | 'Success' | 'Fail';
+type TransactionStatus = "Pending" | "Success" | "Fail";
 type Time = number;
-type ACHType = 'Funding' | 'Redemption';
+type ACHType = "Funding" | "Redemption";
 type Username = string;
 type Hash = string;
 
 export interface UserData {
-	name: string;
-	email: string;
-	dowllaId: string;
-	outstandingBalance: number;
-	lastLogin: Time;
-	blockchainAddress: string;
-	address: string;
-	type: 'Buisness' | 'Personal';
+  name: string;
+  email: string;
+  dowllaId: string;
+  outstandingBalance: number;
+  lastLogin: Time;
+  blockchainAddress: string;
+  address: string;
+  type: "Buisness" | "Personal";
 }
 
 export interface BlockchainData {
-	transactionHash: Hash;
-	from: Hash;
-	to: Hash;
-	fromUser: Username;
-	toUser: Username;
-	type: 'Deposit' | 'Withdraw' | 'Transfer In' | 'Transfer Out';
-	createdAt: Time;
-	amount: number;
-	blocksConfirmed: number;
+  transactionHash: Hash;
+  from: Hash;
+  to: Hash;
+  fromUser: Username;
+  toUser: Username;
+  type: "Deposit" | "Withdraw" | "Transfer In" | "Transfer Out";
+  createdAt: Time;
+  amount: number;
+  blocksConfirmed: number;
 }
 
 export interface ACHData {
-	username: Username;
-	type: ACHType;
-	transactionId: number;
-	createdAt: Time;
-	confirmedAt: Time;
-	amount: number;
-	userBank: string;
-	berksharesBank: string;
-	bankAccount: string;
-	status: TransactionStatus;
+  username: Username;
+  type: ACHType;
+  transactionId: number;
+  createdAt: Time;
+  confirmedAt: Time;
+  amount: number;
+  userBank: string;
+  berksharesBank: string;
+  bankAccount: string;
+  status: TransactionStatus;
 }
 
 export interface IACHTransaction {
-    transactionHash: string;
-    blockNumber: number,
-    timestamp: number;
-    userId: string;
-    operator: string;
-    value: string;
-    type: "Transfer In" | "Transfer Out" | "Deposit" | "Withdraw";
+  transactionHash: string;
+  blockNumber: number;
+  timestamp: number;
+  userId: string;
+  operator: string;
+  value: string;
+  type: "Transfer In" | "Transfer Out" | "Deposit" | "Withdraw";
 }
 
 export interface IBlockchainTransaction {
-	fromUserId: string,
-    fromAddress: string,
-    toUserId: string,
-    toAddress: string,
-    value: string,
-    transactionHash: string,
-    blockNumber: number,
-	timestamp: number,
-	type: "Transfer In" | "Transfer Out" | "Deposit" | "Withdraw" | "Transfer";
+  fromUserId: string;
+  fromAddress: string;
+  toUserId: string;
+  toAddress: string;
+  value: string;
+  transactionHash: string;
+  blockNumber: number;
+  timestamp: number;
+  type: "Transfer In" | "Transfer Out" | "Deposit" | "Withdraw" | "Transfer";
 }
 
 export interface ACHDataState {
-	data: ACHData[]
+  data: ACHData[];
 }
 
 export enum UserTables {
-	UserACHTRansactions,
-	UserBlockchainTransactions
+  UserACHTRansactions,
+  UserBlockchainTransactions,
 }
