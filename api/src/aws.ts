dd0bc00b93f9689cec97288230d1950f7b9832d1
Esraa Jbara <jbesraa@gmail.com>
2022-01-20 14:46:07 +0200

    update to new s3 pkg

--- api/src/aws.ts
+++ api/src/aws.ts
@@ -1,89 +1,76 @@
 import { errors, verifierFactory } from "@southlane/cognito-jwt-verifier";
-import AWS, { AWSError } from "aws-sdk";
-import { Key } from "aws-sdk/clients/iot";
-import {
-  Body,
-  BucketName,
-  PutObjectOutput,
-  ListBucketsOutput,
-  CreateBucketOutput,
-  GetObjectOutput,
-} from "aws-sdk/clients/s3";
-import { PromiseResult } from "aws-sdk/lib/request";
-
-/**AWS Client**/
-
-AWS.config.update({
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+import { S3Client, CreateBucketCommand, ListBucketsCommand, ListBucketsCommandInput, CreateBucketCommandInput, PutObjectCommand, PutObjectCommandInput , GetObjectCommand, GetObjectCommandInput} from "@aws-sdk/client-s3";
 
 /**AWS-S3 Client**/
 
-const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+const s3 = new S3Client({region: "us-west-1"});
 
-export async function listBuckets(): Promise<
-  PromiseResult<ListBucketsOutput, AWSError>
-> {
-  try {
-    const buckets = await s3.listBuckets().promise();
+export async function listBuckets() {
+ try {
+  const input: ListBucketsCommandInput = {};
+  const command = new ListBucketsCommand(input);
+  const buckets = await s3.send(command);
 
-    return buckets;
-  } catch (error) {
-    return error;
-  }
+  return buckets;
+ } catch (error) {
+  throw new Error(error)
+ }
 }
 
 export async function createBucket(
-  bucketName: string
-): Promise<PromiseResult<CreateBucketOutput, AWSError>> {
-  try {
-    const bucketParams = {
-      Bucket: bucketName,
-    };
-    const response = await s3.createBucket(bucketParams).promise();
+ bucketName: string
+){
+ try {
+  const input: CreateBucketCommandInput = {
+   Bucket: bucketName,
+  };
+  const command = new CreateBucketCommand(input);
+  const response = await s3.send(command);
 
-    return response;
-  } catch (error) {
-    return error;
-  }
+  return response;
+ } catch (error) {
+  throw new Error(error)
+ }
 }
 
 export async function uploadFileToBucket(
-  bucketName: BucketName,
-  filePath: Key,
-  fileBody: Body
-): Promise<PromiseResult<PutObjectOutput, AWSError>> {
-  try {
-    const params = {
-      Bucket: bucketName,
-      Key: filePath,
-      Body: fileBody,
-      ContentType: "application/octet-stream",
-      CacheControl: "public, max-age=86400",
-      ACL: "public-read",
-    };
-    const res = await s3.putObject(params).promise();
+ bucketName,
+ filePath,
+ fileBody
+){
+ try {
+  const input: PutObjectCommandInput = {
+   Bucket: bucketName,
+   Key: filePath,
+   Body: fileBody,
+   ContentType: "application/octet-stream",
+   CacheControl: "public, max-age=86400",
+   ContentEncoding: 'base64',
+   ACL: "public-read",
+  };
+  const command = new PutObjectCommand(input)
+  const res = await s3.send(command);
 
-    return res;
-  } catch (error) {
-    return error;
-  }
+  return res;
+ } catch (error) {
+  throw new Error(error)
+ }
 }
 
 export async function getFileFromBucket(
-  bucketName: BucketName,
-  fileName: Key
-): Promise<PromiseResult<GetObjectOutput, Error>> {
-  try {
-    const response = await s3
-      .getObject({ Bucket: bucketName, Key: fileName })
-      .promise();
-    return response;
-  } catch (error) {
-    return error;
+ bucketName,
+ fileName
+){
+ try {
+  const input: GetObjectCommandInput = {
+   Bucket: bucketName, Key: fileName 
   }
+  const command = new GetObjectCommand(input);
+  const response = await s3.send(command);
+  return response;
+ } catch (error) {
+  throw new Error(error)
+ }
 }
 
 /**Merchants CSV Transaction Reports**/
@@ -91,10 +78,10 @@ export async function getFileFromBucket(
 export const MERCHANTS_TX_REPORTS = "merchants-tx-reports";
 
 export async function uploadMerchantReportToS3(
-  filePath: Key,
-  fileBody: Body
+ filePath,
+ fileBody
 ): Promise<void> {
-  await uploadFileToBucket(MERCHANTS_TX_REPORTS, filePath, fileBody);
+ await uploadFileToBucket(MERCHANTS_TX_REPORTS, filePath, fileBody);
 }
 
 /**AWS Token Middleware Verifier**/
@@ -102,31 +89,31 @@ export async function uploadMerchantReportToS3(
 const { JwtVerificationError, JwksNoMatchingKeyError } = errors;
 
 export const cognitoVerifier = (): {
-  verify: (token: string) => Promise<string>;
+ verify: (token: string) => Promise<string>;
 } =>
-  verifierFactory({
-    region: process.env.AWS_REGION,
-    userPoolId: process.env.AWS_POOL_ID,
-    appClientId: process.env.AWS_CLIENT_ID,
-    tokenType: process.env.AWS_TOKEN_TYPE,
-  });
+ verifierFactory({
+ region: process.env.AWS_REGION,
+ userPoolId: process.env.AWS_POOL_ID,
+ appClientId: process.env.AWS_CLIENT_ID,
+ tokenType: process.env.AWS_TOKEN_TYPE,
+});
 
 export const verifyCognitoToken = async (
-  token: string
+ token: string
 ): Promise<{ success: boolean; token: string }> => {
-  try {
-    const verifier = cognitoVerifier().verify;
-    const verifiedToken = await verifier(token);
+ try {
+  const verifier = cognitoVerifier().verify;
+  const verifiedToken = await verifier(token);
 
-    return { success: true, token: verifiedToken };
-  } catch (e) {
-    if (
-      e instanceof JwtVerificationError ||
-      e instanceof JwksNoMatchingKeyError
-    ) {
-      return { success: false, token: token };
-    }
-
-    return { success: false, token: token };
+  return { success: true, token: verifiedToken };
+ } catch (e) {
+  if (
+   e instanceof JwtVerificationError ||
+   e instanceof JwksNoMatchingKeyError
+  ) {
+   return { success: false, token: token };
   }
+
+  return { success: false, token: token };
+ }
 };
