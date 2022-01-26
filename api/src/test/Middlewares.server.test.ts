a232a0dd331ad5b139f4e0ab1d84659eb90ebc8e
Esraa Jbara <jbesraa@gmail.com>
2022-01-26 11:20:58 +0200

    fix tests

--- api/src/test/Middlewares.server.test.ts
+++ api/src/test/Middlewares.server.test.ts
@@ -2,12 +2,15 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 import dotenv from "dotenv";
 import path from "path";
-import * as sinon from "sinon";
-import * as aws from "../aws";
 import { getApp } from "../server";
 import { log } from "../utils";
 import { codes } from "../utils/http";
 import { mockDatabase } from "./setup/setup-db-integration";
+
+const expiredToken =
+  "eyJraWQiOiI0UFFoK0JaVExkRVFkeUM2b0VheVJDckVjblFDSXhqbFZFbTFVd2RhZ2ZNPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiQlNFSWQ1bkYyN3pNck45QkxYLVRfQSIsInN1YiI6IjI0ZTI2OTEwLWU3YjktNGFhZC1hOTk0LTM4Nzk0MmYxNjRlNyIsImF1ZCI6IjVyYTkxaTlwNHRycTQybTJ2bmpzMHB2MDZxIiwiZXZlbnRfaWQiOiJiNmQ3YTYyZC01NGRhLTQ5ZTYtYTgzOS02NjUwNmYwYzIxYjUiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTU4NzMxMTgzOCwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfUERzeTZpMEJmIiwibmFtZSI6Ik1heCBJdmFub3YiLCJjb2duaXRvOnVzZXJuYW1lIjoiMjRlMjY5MTAtZTdiOS00YWFkLWE5OTQtMzg3OTQyZjE2NGU3IiwiZXhwIjoxNTg3MzE1NDM4LCJpYXQiOjE1ODczMTE4MzgsImVtYWlsIjoibWF4QHNvdXRobGFuZS5jb20ifQ.GrlpeYQDwB81HjBZRkuqzw0ZXSGFBi_pbMoWC1QvHyPYrc6NRto02H4xgMls5OmCGa4bZBYWTT6wfo0bxuOLZDP__JRSfOyPUIbiAWTu1IiyAhbt3nlW1xSNSvf62xXQNveF9sPcvG2Gh6-0nFEUrAuI1a5QAVjXbp1YDDMr2TzrFrugW7zl2Ntzj42xWIq7P0R75S2JYVmBfhAxS6YNO1n8KpOFzxagxmn89leledx4PTxuOdWdmT6vZkW9q9QnOI9kjgUIxfWjx55205P4BwkOeqY7AN0j85LBwAHbhezfzNETybX1pwnMBh1p5_iLYgQMMZ60ZJseGl3cMRsPnQ";
+
+jest.setTimeout(3000);
 
 const expect = chai.expect;
 chai.use(chaiHttp);
@@ -21,7 +24,6 @@ if (result.error) {
   throw result.error;
 }
 
-let stub;
 describe("Middlewares", () => {
   beforeAll(async () => {
     await mockDatabase.init();
@@ -31,28 +33,22 @@ describe("Middlewares", () => {
     await mockDatabase.stop();
   });
 
-  afterEach(() => {
-    sinon.restore();
-  });
-
   describe("auth", () => {
     beforeEach(async (): Promise<void> => {
       if (mockDatabase.isConnectionOpen()) return;
       await mockDatabase.openNewMongooseConnection();
     });
 
-    it.skip("Incorrect token", (done) => {
-      stub = sinon.stub(aws, "verifyCognitoToken");
+    it("Incorrect token", (done) => {
       chai
         .request(server)
-        .get("/health")
-        .set("authorization", "tokeeeen")
+        .post("/users")
+        .set("authorization", expiredToken)
         .then((res) => {
           expect(res).to.have.status(codes.UNAUTHORIZED);
           expect(JSON.parse(res.text)).to.eql({
             message: "User is Unauthorized",
           });
-          expect(stub.calledOnce).to.eql(true);
           log(JSON.parse(res.text));
           done();
         })
@@ -62,17 +58,15 @@ describe("Middlewares", () => {
     });
   });
 
-  it.skip("Token does not exist", (done) => {
-    stub = sinon.stub(aws, "verifyCognitoToken");
+  it("Token does not exist", (done) => {
     chai
       .request(server)
-      .get("/health")
+      .post("/users")
       .then((res) => {
         expect(res).to.have.status(codes.UNAUTHORIZED);
         expect(JSON.parse(res.text)).to.eql({
           message: "No Auth Headers In Request",
         });
-        expect(stub.calledOnce).to.eql(false);
         log(JSON.parse(res.text));
         done();
       })
