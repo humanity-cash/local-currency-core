bf95af50147ed8aedf259746aee1e37b809ab15e
Aaron Boyd <aaronmboyd@gmail.com>
2022-02-02 13:08:47 +0100

    [STYLE] - Add ellipse mask to Values images

--- api/src/content/Values.ts
+++ api/src/content/Values.ts
@@ -4,35 +4,35 @@ export const Values: HomeScreenContent[] = [
   {
     contentType: "Values",
     image:
-      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsWell_original.tif.png",
+      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsWell_original.tif.png?mask=ellipse",
     textTitle: "COMMUNITY",
     text: "BerkShares fosters stronger community connections through personal exchanges and local trade.",
   },
   {
     contentType: "Values",
     image:
-      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsWheat_original.tif.png",
+      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsWheat_original.tif.png?mask=ellipse",
     textTitle: "ECONOMY",
     text: "BerkShares are a tool for strengthening the local economy by prioritizing local exchange, building regional supply chain resilience.",
   },
   {
     contentType: "Values",
     image:
-      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsHive_original.tif.png",
+      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsHive_original.tif.png?mask=ellipse",
     textTitle: "ECOLOGY",
     text: "By encouraging people to shop locally, and businesses to procure locally, BerkShares decreases the distance of goods shipped.",
   },
   {
     contentType: "Values",
     image:
-      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsPhoenix_original.tif.png",
+      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsPhoenix_original.tif.png?mask=ellipse",
     textTitle: "SUSTAINABILITY",
     text: "By promoting the local production and consumption of food and manufactured wares, using BerkShares helps nurture a sustainable future for the Berkshires.",
   },
   {
     contentType: "Values",
     image:
-      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsPeacock_original.tif.png",
+      "https://berkshares-images.imgix.net/BSHARES_UI_AssetsPeacock_original.tif.png?mask=ellipse",
     textTitle: "EQUITY",
     text: "BerkShares strives to support a flourishing local economy for all residents, including groups structurally excluded from economic determination and regional prosperity.",
   },
