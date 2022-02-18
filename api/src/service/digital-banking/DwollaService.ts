80d83095dc27a0b86f6eccabf793eec3423849f0
Aaron Boyd <aaronmboyd@gmail.com>
2022-02-18 12:11:08 +0100

    [FEAT] - Deposit reconciliation on startup

--- api/src/service/digital-banking/DwollaService.ts
+++ api/src/service/digital-banking/DwollaService.ts
@@ -7,7 +7,10 @@ import {
 } from "./DwollaTypes";
 import { IDwollaNewUserResponse } from "../../types";
 import { isDwollaProduction, log, httpUtils } from "src/utils";
-import { getAppToken, getIdempotencyHeader } from "./DwollaUtils";
+import { getAppToken, getDwollaResourceFromLocation, getIdempotencyHeader } from "./DwollaUtils";
+import { DwollaTransferService } from "src/database/service";
+import { getWallet } from "../PublicService";
+import { webhookMint } from "../OperatorService";
 
 export async function getDwollaCustomerById(
   id: string
@@ -188,5 +191,102 @@ export async function createUnverifiedCustomer(
   } catch (e) {
     log("DwollaService.ts::createUnverifiedCustomer(), error " + e);
     throw e;
+  }
+}
+
+export async function reconcileDwollaDeposits() : Promise<boolean> {
+
+  try{
+    
+    const deposits : DwollaTransferService.IDwollaTransferDBItem[] = await DwollaTransferService.getAll("DEPOSIT");
+    
+    for(let i = 0;i < deposits?.length;i++){
+      
+      try{
+      
+        const deposit = deposits[i];
+        const createdDate = new Date(deposit.created).toLocaleTimeString();
+        const updatedDate = new Date(deposit.updated).toLocaleTimeString();
+        const user = await getWallet(deposit.userId);
+        
+        log(`**************************************************************************************************************`);
+        log(`reconcileDwollaDeposits() Reconciling deposit for userId ${deposit.userId} (${user.customer?.body?.firstName} ${user.customer?.body?.lastName} ${user.customer?.body?.email}) for ${deposit.amount}, created ${createdDate}, updated ${updatedDate}`);
+        log(`reconcileDwollaDeposits() Database status - fundingTransferId: ${deposit.fundingTransferId} fundingTransferStatus: ${deposit.fundingStatus}`);
+        log(`reconcileDwollaDeposits() Database status - fundedTransferId: ${deposit.fundedTransferId} fundedTransferStatus: ${deposit.fundedStatus}`);
+        log(`txId: ${deposit.txId}`);
+        
+        const fundingStatusCompleteLocal = deposit.fundingStatus?.includes("completed");
+        const fundedStatusCompleteLocal = deposit.fundedStatus?.includes("completed");
+
+        let dwollaFundingTransfer : dwolla.Response;
+        if(deposit.fundingTransferId)
+          dwollaFundingTransfer = await getDwollaResourceFromLocation(`${process.env.DWOLLA_BASE_URL}transfers/${deposit.fundingTransferId}`);
+
+        let dwollaFundedTransfer : dwolla.Response;
+        if(deposit.fundedTransferId)
+          dwollaFundedTransfer = await getDwollaResourceFromLocation(`${process.env.DWOLLA_BASE_URL}transfers/${deposit.fundedTransferId}`);
+
+        const fundingStatusCompleteDwolla = dwollaFundingTransfer?.body?.status?.includes("processed");
+        const fundedStatusCompleteDwolla = dwollaFundedTransfer?.body?.status?.includes("processed");
+
+        log(`reconcileDwollaDeposits() Dwolla status - fundingTransferId: ${deposit.fundingTransferId} fundingTransferStatus: ${dwollaFundingTransfer?.body?.status}`);
+        log(`reconcileDwollaDeposits() Dwolla status - fundedTransferId: ${deposit.fundedTransferId} fundedTransferStatus: ${dwollaFundedTransfer?.body?.status}`);
+
+        let shouldMint = false;
+
+        if(fundingStatusCompleteDwolla && fundedStatusCompleteDwolla){
+          log(`reconcileDwollaDeposits() This deposit is fully processed by Dwolla...`);
+          
+          if(fundedStatusCompleteLocal && fundingStatusCompleteLocal){
+            log(`reconcileDwollaDeposits() This deposit is fully processed by us...`);
+            
+            if(deposit.txId){
+              log(`reconcileDwollaDeposits() This deposit appears to have been processed on-chain and minted, deposit is fully reconciled! No further action required.`);
+            }
+            else{
+              log(`reconcileDwollaDeposits() This deposit appears to have been fully completed both on Dwolla and in our database, but not minted. Proceed with minting.`);
+              shouldMint = true;
+            }
+          }
+          else {
+            log(`reconcileDwollaDeposits() This deposit appears fully processed by Dwolla but this is not reflected in our database, perhaps we missed a webhook?`);
+
+            if(deposit.fundingTransferId && deposit.fundedTransferId){
+              await DwollaTransferService.updateStatusByFundingTransferId(deposit.fundingTransferId, "transfer_completed");
+              log(`reconcileDwollaDeposits() Updated fundingStatus to "transfer_completed"`);
+              await DwollaTransferService.updateStatusByFundedTransferId(deposit.fundedStatus, "bank_transfer_completed");
+              log(`reconcileDwollaDeposits() Updated fundedStatus to "bank_transfer_completed"`);
+              shouldMint = true;
+            }
+            else{
+              throw `fundingTransferId and fundedTransferId are not both set in the database so we cannot update both statuses`;
+            }
+          }
+        }
+        else{
+          log(`reconcileDwollaDeposits() This deposit is not fully processed by Dwolla, continue waiting...`);
+        }
+
+        if(shouldMint){
+          const success = await webhookMint(deposit.fundingTransferId);
+          if(success){
+            const transfer: DwollaTransferService.IDwollaTransferDBItem = await DwollaTransferService.getByFundingTransferId(deposit.fundingTransferId); 
+            log(`reconcileDwollaDeposits() Successfully minted on-chain for deposit with fundingTransferId ${deposit.fundingTransferId} for user ${deposit.userId}, txId: ${transfer.txId}`);
+
+          }
+          else
+            throw `Could not mint! webhookMint returned false!`
+        }
+      }
+      catch(err){
+        log(`reconcileDwollaDeposits() Error processing this deposit: ${err}`);
+        log(`reconcileDwollaDeposits() Skipping this deposit and moving to the next one. Requires manual investigation.`);
+      }
+    }
+    return true;
+  }
+  catch(err){
+    log(`reconcileDwollaDeposits() Critical error ${err} during deposit reconciliation...`);
+    return false;  
   }
 }
