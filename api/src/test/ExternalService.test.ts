af83f0785a8476618be66a24839e6ec2fd037c90
Aaron Boyd <aaronmboyd@gmail.com>
2022-02-27 16:38:17 +0100

    [STYLE] - Prettier

--- api/src/test/ExternalService.test.ts
+++ api/src/test/ExternalService.test.ts
@@ -10,13 +10,13 @@ chai.use(chaiHttp);
 const server = getApp();
 
 describe("External service test", () => {
-  
   describe("POST /user/:id/cache/profilePicture", () => {
-
     it("it should purge a known image URL", (done) => {
       chai
         .request(server)
-        .post("/users/953408b2-98e4-40f1-8138-1701ad954f36/cache/profilePicture")
+        .post(
+          "/users/953408b2-98e4-40f1-8138-1701ad954f36/cache/profilePicture"
+        )
         .then((res) => {
           log(JSON.parse(res.text));
           expect(res).to.have.status(codes.OK);
@@ -29,17 +29,17 @@ describe("External service test", () => {
 
     // This actually succeeds on Imgix
     xit("it should fail to purge an unknown image URL", (done) => {
-        chai
-          .request(server)
-          .post("/users/bananas/cache/profilePicture")
-          .then((res) => {
-            log(JSON.parse(res.text));
-            expect(res).to.have.status(codes.NOT_FOUND);
-            done();
-          })
-          .catch((err) => {
-            done(err);
-          });
-      });
+      chai
+        .request(server)
+        .post("/users/bananas/cache/profilePicture")
+        .then((res) => {
+          log(JSON.parse(res.text));
+          expect(res).to.have.status(codes.NOT_FOUND);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
   });
 });
