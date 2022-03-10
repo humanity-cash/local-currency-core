81e12f5882c94afc85bd3651c8b0826bd27515ae
Aaron Boyd <aaronmboyd@gmail.com>
2022-03-10 12:57:36 +0100

    [FEAT] - Allow RedemptionFee to be visible as a transfer item

--- api/src/utils/text.ts
+++ api/src/utils/text.ts
@@ -1,2 +1,3 @@
 export const HUMANITY_CASH_DISPLAY_NAME = "BerkShares Bonus";
 export const COMMUNITY_CHEST_DISPLAY_NAME = "Community Chest";
+export const HUMANITY_CASH_REDEMPTION_FEE_DISPLAY_NAME = "Humanity Cash Redemption Fee";
