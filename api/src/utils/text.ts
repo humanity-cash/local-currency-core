87fe055f042cd694cb568ac171f19fb23a287d91
Aaron Boyd <aaronmboyd@gmail.com>
2022-03-10 12:58:19 +0100

    [STYLE] - Prettier

--- api/src/utils/text.ts
+++ api/src/utils/text.ts
@@ -1,3 +1,4 @@
 export const HUMANITY_CASH_DISPLAY_NAME = "BerkShares Bonus";
 export const COMMUNITY_CHEST_DISPLAY_NAME = "Community Chest";
-export const HUMANITY_CASH_REDEMPTION_FEE_DISPLAY_NAME = "Humanity Cash Redemption Fee";
+export const HUMANITY_CASH_REDEMPTION_FEE_DISPLAY_NAME =
+  "Humanity Cash Redemption Fee";
