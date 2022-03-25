b8d550c23a43a370ff446e11056400ec9fd47832
Aaron Boyd <aaronmboyd@gmail.com>
2022-03-25 12:12:03 +0100

    [STYLE] - Email feedback changes

--- api/src/test/AWSSESEmail.test.ts
+++ api/src/test/AWSSESEmail.test.ts
@@ -6,19 +6,19 @@ import { DepositEmailTemplate, WithdrawalEmailTemplate, sendTemplatedEmail} from
 const expect = chai.expect;
 
 describe("Email testing", () => {
-    it("it to send a DepositCompleted template email", async () => {
+    it("should send a DepositCompleted template email", async () => {
       const params : DepositEmailTemplate = {
         amount: "1.23",
         userId: v4(),
         transactionId: v4(),
         timestamp: new Date().toLocaleString(),
-        randomness: v4()
+        randomness: v4() //required so Gmail doesn't bundle the emails and trim the footer
       }
       const success = await sendTemplatedEmail("DepositCompleted", params, "tech@humanity.cash");
       expect(success);
     });
 
-    it("it to send a WithdrawalCompleted template email", async () => {
+    it("should send a WithdrawalCompleted template email", async () => {
       const params : WithdrawalEmailTemplate = {
         amount: "94.56",
         userId: v4(),
@@ -26,7 +26,7 @@ describe("Email testing", () => {
         timestamp: new Date().toLocaleString(),
         feeAmount: "0.50",
         netAmount: "94.06",
-        randomness: v4()
+        randomness: v4() //required so Gmail doesn't bundle the emails and trim the footer
       }
       const success : boolean  = await sendTemplatedEmail("WithdrawalCompleted", params, "tech@humanity.cash");
       expect(success);
