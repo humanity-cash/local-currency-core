54509e501685a48027b7ef98a8958470222d5520
Aaron Boyd <aaronmboyd@gmail.com>
2022-04-11 16:21:10 +0200

    [FEAT] - Add blocklist for business directory

--- api/src/router/businesses/controller.ts
+++ api/src/router/businesses/controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { getBusinesses } from "src/database/service/User";
-import { httpUtils } from "src/utils";
+import { httpUtils, blockList } from "src/utils";
 import { Business } from "src/types";
 
 const codes = httpUtils.codes;
@@ -10,7 +10,17 @@ export async function getAllBusinesses(
   res: Response
 ): Promise<void> {
   try {
-    const businesses: Business[] = await getBusinesses();
+    let businesses: Business[] = await getBusinesses();
+
+    // Filter with block list businesses
+    // Test accounts, accounts accidentally created
+    // or businesses that have are known to not be 
+    // in the Berkshires region
+    if(businesses?.length > 0){
+      const blockListDwollaIds : string[] = blockList.BUSINESS_BLOCKLIST.map(x => x.dwollaUserId);
+      businesses = businesses.filter(element => !blockListDwollaIds.includes(element.dwollaId));      
+    }
+
     httpUtils.createHttpResponse(businesses, codes.OK, res);
   } catch (err) {
     httpUtils.serverError(err, res);
