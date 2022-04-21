f932985f9f0cf9ab5c20f23c9faaa05f76f48c29
JeyDev310 <mtroz310@gmail.com>
2022-04-21 10:59:33 -0400

    welcome eamil moved to webhook

--- api/src/service/digital-banking/DwollaWebhookService.ts
+++ api/src/service/digital-banking/DwollaWebhookService.ts
@@ -32,6 +32,7 @@ import {
   WithdrawalEmailTemplate,
 } from "src/aws";
 import { v4 } from "uuid";
+import { WelcomeEmailTemplate } from 'src/aws';
 
 export async function deregisterWebhook(
   webhookUrl: string
@@ -611,6 +612,20 @@ export async function consumeWebhook(
           log(
             `DwollaWebhookService.ts::consumeWebhook() Address ${address} created on-chain for userId ${customer.id}`
           );
+
+          const params: WelcomeEmailTemplate = {
+            randomness: v4(), //required so Gmail doesn't bundle the emails and trim the footer
+          }
+          const emailSuccess = await sendTemplatedEmail(
+            "AccountCreated",
+            params,
+            customer.email
+          );
+          if (!emailSuccess)
+            log(
+              `Warning: account created but welcome email could not be sent to ${customer.email}`
+            );
+        
           processed = true;
         } catch (err) {
           log(
