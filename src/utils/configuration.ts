3e4db8b4166e5e3864bb1edf7491f9827635b5a8
Aaron Boyd <aaronmboyd@gmail.com>
2022-05-19 17:31:42 +0200

    [FIX] - Use managed secrets by default

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -29,34 +29,65 @@ async function getSecrets(secretName: string, region: string): Promise<string> {
 
 export async function configureEnvironment(): Promise<void> {
   console.log(
-    `Retrieving ${process.env.SECRET_NAME} from region ${process.env.REGION}`
+    `Retrieving ${process.env.SECRET_NAME} from region ${process.env.AWS_SECRET_REGION}`
   );
 
-  const secrets = await getSecrets(process.env.SECRET_NAME, process.env.REGION);
+  const secrets = await getSecrets(process.env.SECRET_NAME, process.env.AWS_SECRET_REGION);
   const secretsParsed = JSON.parse(secrets);
-  console.log(secretsParsed);
 
   const configurationItems: string[] = [
     "DEBUG",
     "NODE_ENV",
     "PORT",
+    
     "LOCAL_CURRENCY_RPC_HOST",
     "LOCAL_CURRENCY_ADDRESS",
     "LOCAL_CURRENCY_MNEMONIC",
-    "DERIVATION_PATH",
-    "NUMBER_OPERATORS",
-    "OPERATOR_1_FUNDING_SOURCE",
-    "OPERATOR_2_FUNDING_SOURCE",
-    "AWS_REGION",
-    "AWS_POOL_ID",
-    "AWS_CLIENT_ID",
-    "AWS_TOKEN_TYPE",
+    "LOCAL_CURRENCY_MNEMONIC_INDEX",
+
     "DWOLLA_BASE_URL",
     "DWOLLA_APP_KEY",
     "DWOLLA_APP_SECRET",
     "DWOLLA_ENVIRONMENT",
     "WEBHOOK_SECRET",
     "WEBHHOK_URL",
+
+    "REGISTER_WEBHOOK",
+    "SIMULATE_WEBHOOK",
+    "SIMULATE_BANKING",
+    "DELETE_PRIOR_WEBHOOK",
+
+    "USE_MONGO_TLS",
+    "MONGO_DB_USER",
+    "MONGO_DB_PASSWORD",
+    "MONGO_URL",
+
+    "NUMBER_OPERATORS",
+    "CUSTOMER_WITHDRAWAL_BALANCE_LIMIT",
+
+    "OPERATOR_1_DWOLLA_USER_ID",
+    "OPERATOR_1_MNEMONIC",
+    "OPERATOR_1_MNEMONIC_INDEX",
+    "OPERATOR_1_DISPLAY_NAME",
+    "OPERATOR_1_FUNDING_SOURCE",
+
+    "OPERATOR_2_DWOLLA_USER_ID",
+    "OPERATOR_2_MNEMONIC",
+    "OPERATOR_2_MNEMONIC_INDEX",
+    "OPERATOR_2_DISPLAY_NAME",
+    "OPERATOR_2_FUNDING_SOURCE",
+
+    "AWS_REGION",
+    "AWS_POOL_ID",
+    "AWS_CLIENT_ID",
+    "AWS_TOKEN_TYPE",
+
+    "IMGIX_PROFILE_PICTURE_URL",
+    "IMGIX_PURGE_API",
+    "IMGIX_API_TOKEN",
+
+    "TRANSFER_RECONCILE_ON_STARTUP",
+    "PROMOTION_RECONCILE_ON_STARTUP"
   ];
 
   configurationItems.forEach((element) => {
